3214e3f1b8f95a92e8cbd9a6e4e275d6427fd7cd
mxie32 <mxie32@gatech.edu>
2018-12-21 23:15:17 -0500

    temp save

--- src/test_forward.py
+++ src/test_forward.py
@@ -21,54 +21,75 @@ def helper_calculate_twist_i(i_T_i_minus_1, joint_vel_i, twist_i_mius_1, screw_a
     return twist_i
 
 
-def forward_traditional_way_R():
+def forward_traditional_way_RR():
     # Calculate joint accelerations for R manipulator with traditional method
-    return vector(0)
+    return vector(0,0)
 
 
-def forward_factor_graph_way_R():
+def forward_factor_graph_way_RR():
     # Setup factor graph
     # Optimize it
     # return joint accelerations
 
     # number of revolute joint
-    num = 1
+    num = 2
 
     twist_i_mius_1 = vector(0., 0., 0., 0., 0., 0.)
     # configuration of link 0 frame in space frame 0
     pose3_0 = Pose3(Rot3(np.identity(3)), Point3(0,0,0))
     # configuration of link 1 frame in space frame 0
     pose3_1 = Pose3(Rot3(np.identity(3)), Point3(1,0,0))
+    # configuration of link 1 frame in space frame 0
+    pose3_2 = Pose3(Rot3(np.identity(3)), Point3(3,0,0))
     # configuration of end effector frame in space frame 0
-    pose3_2 = Pose3(Rot3(np.identity(3)), Point3(2,0,0))
-    link_config = np.array([pose3_0, pose3_1, pose3_2])
+    pose3_eef = Pose3(Rot3(np.identity(3)), Point3(4,0,0))
+    link_config = np.array([pose3_0, pose3_1, pose3_2, pose3_eef])
 
     # joint velocity of link 1
-    joint_vel = np.array([0.0, 1.0])
+    joint_vel = np.array([0.0, 1.0, 1.0])
 
     # screw axis for joint 0 expressed in link frame 0
     screw_axis_0 = unit_twist(vector(0,0,0), vector(0,0,0))
     # screw axis for joint 1 expressed in link frame 1
     screw_axis_1 = unit_twist(vector(0,0,1), vector(-1,0,0))
-    screw_axis = np.array([screw_axis_0, screw_axis_1])
+    # screw axis for joint 2 expressed in link frame 2
+    screw_axis_2 = unit_twist(vector(0,0,1), vector(-1,0,0))
+    screw_axis = np.array([screw_axis_0, screw_axis_1, screw_axis_2])
 
     # inertial matrix of link i expressed in link frame i
     I0 = np.zeros((3,3))
     I1 = np.diag([0, 1/6., 1/6.])
-    I = np.array([I0, I1])
+    I2 = np.diag([0, 1/6., 1/6.])
+    I = np.array([I0, I1, I2])
     # mass of link i
     m0 = 0
     m1 = 1
-    m = np.array([m0, m1])
+    m2 = 1
+    m = np.array([m0, m1, m2])
 
     # Gaussian Factor Graph
     gfg = GaussianFactorGraph()
+
+    # factor graph keys
+    key_twist_accel_i_minus_1 = 1
+    key_twist_accel_i = 2
+    key_joint_accel_i = 3
+    key_wrench_i = 4
+    key_wrench_i_plus_1 = 5
 
     for i in range(1, num+1):
 
         # configuration of link frame i-1 relative to link frame i for joint i angle 0 
         i_T_i_minus_1 = compose(link_config[i].inverse(), link_config[i-1])
         twist_i = helper_calculate_twist_i(i_T_i_minus_1, joint_vel[i], twist_i_mius_1, screw_axis[i])
+
+        if i is not 1:
+            key_twist_accel_i = key_wrench_i + 1
+            key_joint_accel_i = key_wrench_i + 2
+            key_wrench_i_plus_1 = key_wrench_i + 3
+        
+        print(key_twist_accel_i_minus_1, key_twist_accel_i,
+              key_joint_accel_i, key_wrench_i, key_wrench_i_plus_1)
 
         ## factor 1
         # LHS of acceleration equation 
@@ -82,10 +103,6 @@ def forward_factor_graph_way_R():
                                      [screw_axis[i][5]]])
         # RHS of acceleration equation 
         b_accel = np.dot(adtwist(twist_i), screw_axis[i]*joint_vel[i])
-
-        key_twist_accel_i_minus_1 = 1 + 5*(i - 1)
-        key_twist_accel_i = 2 + 5*(i - 1)
-        key_joint_accel_i = 3 + 5*(i - 1)
 
         sigmas = np.zeros(6)
         model = noiseModel_Diagonal.Sigmas(sigmas)
@@ -105,9 +122,6 @@ def forward_factor_graph_way_R():
         # RHS of wrench equation
         b_wrench = -np.dot(np.dot(adtwist(twist_i).transpose(), genaral_mass_matrix(I[i], m[i])), twist_i)
 
-        key_wrench_i = 4 + 5*(i - 1)
-        key_wrench_i_plus_1 = 5 + 5*(i - 1)
-
         sigmas = np.zeros(6)
         model = noiseModel_Diagonal.Sigmas(sigmas)
         gfg.add(key_wrench_i, J_wrench_i, 
@@ -124,7 +138,7 @@ def forward_factor_graph_way_R():
         gfg.add(key_wrench_i, J_wrench_i, b_torque, model)
 
         if i == 1:
-            ## factor 4, prior factor, link 0 twist acceleration = 0
+            ## prior factor, link 0 twist acceleration = 0
             # LHS 
             J_twist_accel_i_mius_1 = np.identity(6)
             # RHS
@@ -134,7 +148,7 @@ def forward_factor_graph_way_R():
             gfg.add(key_twist_accel_i_minus_1, J_twist_accel_i_mius_1, b_twist_accel, model)
 
         if i == num:
-            ## factor 5, prior factor, end effector wrench = 0
+            ## prior factor, end effector wrench = 0
             # LHS 
             J_wrench_i_plus_1 = np.identity(6)
             # RHS
@@ -144,9 +158,12 @@ def forward_factor_graph_way_R():
             gfg.add(key_wrench_i_plus_1, J_wrench_i_plus_1, b_wrench, model)
 
         twist_i_mius_1 = twist_i
-        results = gfg.optimize()
-        print(results)
-    return vector(0)
+        key_twist_accel_i_minus_1 = key_twist_accel_i
+        key_wrench_i = key_wrench_i_plus_1
+
+    results = gfg.optimize()
+    print(results)
+    return vector(0,0)
 
 class TestForwardDynamics(unittest.TestCase):
     """Unit tests for R manipulator class."""
@@ -165,9 +182,9 @@ class TestForwardDynamics(unittest.TestCase):
 
     def test_R_forward_dynamics(self):
         """Try a simple R robot."""
-        expected_joint_accels = forward_traditional_way_R()
+        expected_joint_accels = forward_traditional_way_RR()
         # Call a function with appropriate arguments to co compute them 
-        actual_joint_accels = forward_factor_graph_way_R()
+        actual_joint_accels = forward_factor_graph_way_RR()
         np.testing.assert_array_almost_equal(actual_joint_accels, expected_joint_accels)
 
 
