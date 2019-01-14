a9bfbda2f4807831c1c4bcdf73e1bf1633ff8a28
mxie32 <mxie32@gatech.edu>
2019-01-13 22:16:19 -0500

    create link parameters class

--- src/test_forward.py
+++ src/test_forward.py
@@ -16,25 +16,26 @@ from gtsam import Point3, Pose3, Rot3, symbol
 import utils
 from utils import vector, GtsamTestCase
 
-import manipulator_paras
+import link_parameters
 
-def forward_traditional_way(manipulator_type):
+def forward_traditional_way(paras):
     """
     Calculate joint accelerations for manipulator (Puma robot arm)
     with 6 revolute joints using traditional method 
     Return joint accelerations
-    arguments: manipulator_type:
-               puma manipulator or RR link manipulator
+    arguments: paras:
+               Denavit-Hartenberg parameters and
+               input values for PUMA manipulator
     """
-    if manipulator_type == 'RR':
-        return manipulator_paras.RR_paras['dh_tdd'][1:-1]
-    else:
-        return manipulator_paras.puma_paras['dh_tdd'][1:-1] 
+    return paras.dh_tdd[1:-1] 
 
 def get_link_configuration(num_of_links, paras):
     """
     return each link frame (origined at center of mass) 
     expressed in space frame
+    arguments: paras:
+               Denavit-Hartenberg parameters and
+               input values for PUMA manipulator
     """
     link_config = np.array(Pose3(Rot3(), Point3(0, 0, 0)))
     # link i-1 joint frame expressed in space frame s
@@ -42,22 +43,22 @@ def get_link_configuration(num_of_links, paras):
 
     for i in range(1, num_of_links+1):
         # link i joint frame expressed in link i-1 joint frame
-        joint_i_minus_1_frame_joint_i = utils.compose(Pose3(Rot3.Roll(paras['dh_f'][i-1]), Point3(paras['dh_a'][i-1], 0, 0)),
-                                           Pose3(Rot3.Yaw(paras['dh_t'][i]), Point3(0, 0, paras['dh_d'][i])))
+        joint_i_minus_1_frame_joint_i = utils.compose(Pose3(Rot3.Roll(paras.dh_f[i-1]), Point3(paras.dh_a[i-1], 0, 0)),
+                                           Pose3(Rot3.Yaw(paras.dh_t[i]), Point3(0, 0, paras.dh_d[i])))
         # link i joint frame expressed in space frame s
         frame_joint_i = utils.compose(frame_joint_i_minus_1, joint_i_minus_1_frame_joint_i)
         # update link i-1 joint frame in space frame s for next iteration
         frame_joint_i_minus_1 = frame_joint_i
         # link i com frame expressed in space frame s 
-        frame_i = utils.compose(frame_joint_i, Pose3(Rot3(), Point3(paras['dh_com'][0, i],
-                                                                    paras['dh_com'][1, i], 
-                                                                    paras['dh_com'][2, i])))
+        frame_i = utils.compose(frame_joint_i, Pose3(Rot3(), Point3(paras.dh_com[0, i],
+                                                                    paras.dh_com[1, i], 
+                                                                    paras.dh_com[2, i])))
         link_config = np.append(link_config, frame_i)
 
     # configuration of link eef frame expressed in space frame num_of_links
     # eef frame expressed in last link joint frame
-    joint_last_frame_eef = utils.compose(Pose3(Rot3.Roll(paras['dh_f'][num_of_links]), Point3(paras['dh_a'][num_of_links], 0, 0)),
-                                              Pose3(Rot3.Yaw(paras['dh_t'][num_of_links+1]), Point3(0, 0, paras['dh_d'][num_of_links+1])))
+    joint_last_frame_eef = utils.compose(Pose3(Rot3.Roll(paras.dh_f[num_of_links]), Point3(paras.dh_a[num_of_links], 0, 0)),
+                                              Pose3(Rot3.Yaw(paras.dh_t[num_of_links+1]), Point3(0, 0, paras.dh_d[num_of_links+1])))
     # eef frame expressed in space frame s
     frame_eef = utils.compose(frame_joint_i_minus_1, joint_last_frame_eef)
     link_config = np.append(link_config, frame_eef)
@@ -66,14 +67,17 @@ def get_link_configuration(num_of_links, paras):
 def get_screw_axis(num_of_links, paras):
     """
     return screw axis of each joints expressed in its own link frame
+    arguments: paras:
+               Denavit-Hartenberg parameters and
+               input values for PUMA manipulator    
     """
     screw_axis = np.array([utils.unit_twist(vector(0, 0, 0), vector(0, 0, 0))])
     for i in range(1, num_of_links+1):
         # screw axis for joint i expressed in link frame i
         screw_axis = np.append(screw_axis, [utils.unit_twist(vector(0, 0, 1),
-                                                            vector(-paras['dh_com'][0, i],
-                                                                   -paras['dh_com'][1, i], 
-                                                                   -paras['dh_com'][2, i]))], axis=0)    
+                                                            vector(-paras.dh_com[0, i],
+                                                                   -paras.dh_com[1, i], 
+                                                                   -paras.dh_com[2, i]))], axis=0)    
     return screw_axis
 
 class forward_factor_graph_way():
@@ -81,34 +85,31 @@ class forward_factor_graph_way():
         Calculate forward dynamics for manipulator (Puma robot arm)
         with 6 revolute joints using factor graph method
     """
-    def __init__(self, manipulator_type):
+    def __init__(self, paras):
         """
         Constructor that initializes forward factor graph
         for dynamics of manipulator (Puma robot arm or RR)
         with only revolute joints
-        arguments: manipulator_type:
-                   puma manipulator or RR link manipulator
+        arguments: paras:
+                   Denavit-Hartenberg parameters and
+                   input values for PUMA manipulator
         """
-        if manipulator_type == 'RR':
-            paras = manipulator_paras.RR_paras
-        else:
-            paras = manipulator_paras.puma_paras
         # number of revolute joint
-        self.num_of_links = paras['dh_d'].size - 2
+        self.num_of_links = paras.dh_d.size - 2
         # configuration of base com frame in space frame s
         self.link_config = get_link_configuration(self.num_of_links, paras)
         # screw axis for joint 0 expressed in link frame 0
         self.screw_axis = get_screw_axis(self.num_of_links, paras)
         # joint velocities (1 rad/s) of joint between link i-1 and link i
-        self.joint_vel = paras['dh_td'][:-1]
+        self.joint_vel = paras.dh_td[:-1]
         # mass of each link
-        self.m = paras['dh_m'][:-1]
+        self.m = paras.dh_m[:-1]
         # inertial matrix of link i expressed in link frame i
-        self.I = paras['dh_pI'][:, :-1]
+        self.I = paras.dh_pI[:, :-1]
         # twist of link 0
         self.twist_i_mius_1 = vector(0, 0, 0, 0, 0, 0)
         # torque applied to each link
-        self.torque = paras['dh_tq']
+        self.torque = paras.dh_tq
 
     def twist_accel_factor(self, i_T_i_minus_1, i):
         """
@@ -258,7 +259,7 @@ class forward_factor_graph_way():
 
 
 class TestForwardDynamics(GtsamTestCase):
-    """Unit tests for R manipulator class."""
+    """Unit tests for different manipulators class."""
 
     def setUp(self):
         """setup."""
@@ -266,13 +267,23 @@ class TestForwardDynamics(GtsamTestCase):
 
     def test_RR_forward_dynamics(self):
         """Try a simple RR robot."""
-        expected_joint_accels = forward_traditional_way('puma')
+        RR = link_parameters.LinkParameters_RR()
+        expected_joint_accels = forward_traditional_way(RR)
         # Call a function with appropriate arguments to co compute them
-        ffg_way = forward_factor_graph_way('puma')
+        ffg_way = forward_factor_graph_way(RR)
         actual_joint_accels = ffg_way.factor_graph_optimization()
         np.testing.assert_array_almost_equal(
             actual_joint_accels, expected_joint_accels)
 
+    def test_PUMA_forward_dynamics(self):
+        """Try a PUMA robot."""
+        PUMA = link_parameters.LinkParameters_PUMA()
+        expected_joint_accels = forward_traditional_way(PUMA)
+        # Call a function with appropriate arguments to co compute them
+        ffg_way = forward_factor_graph_way(PUMA)
+        actual_joint_accels = ffg_way.factor_graph_optimization()
+        np.testing.assert_array_almost_equal(
+            actual_joint_accels, expected_joint_accels)
 
 if __name__ == "__main__":
     unittest.main()
