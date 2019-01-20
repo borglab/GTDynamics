35598b4e77087115cebcaad28fe85de8d16070e7
Frank Dellaert <dellaert@gmail.com>
2019-01-20 00:13:30 -0500

    Specify units

--- src/link.py
+++ src/link.py
@@ -8,6 +8,7 @@ Author: Frank Dellaert and Mandy Xie
 import math
 
 import gtsam
+import numpy as np
 import utils
 
 
@@ -19,14 +20,15 @@ class Link(object):
     def __init__(self, theta, d, a, alpha, joint_type, mass, center_of_mass, inertia):
         """ Constructor.
             Keyword arguments:
-                d (float)               -- link offset, i.e., distance between two joints along joint axis
-                theta (float)           -- angle between two joint frame x-axes (theta)
-                a (float)               -- link length. i.e., distance between two joints
-                alpha (float)           -- link twist, i.e., angle between joint axes
+                d (m)                   -- link offset, i.e., distance between two joints along joint axis
+                theta (degrees)         -- angle between two joint frame x-axes (theta)
+                a (m)                   -- link length. i.e., distance between two joints
+                alpha (degrees)         -- link twist, i.e., angle between joint axes
                 joint_type (char)       -- 'R': revolute,  'P' prismatic
                 mass (float)            -- mass of link
                 center_of_mass (Point3) -- center of mass location expressed in link frame
                 inertia (vector)        -- principal inertias
+            Note: angles are given in degrees, but converted to radians internally.
         """
         self._d = d
         self._theta = math.radians(theta)
@@ -69,3 +71,10 @@ class Link(object):
     def inertia(self):
         """Return link moments of inertia."""
         return self._inertia
+
+    def inertia_matrix(self):
+        """Return the general mass matrix"""
+        gmm = np.zeros((6, 6), np.float)
+        gmm[:3, :3] = np.diag(self.inertia)
+        gmm[3:, 3:] = self.mass * np.identity(3)
+        return gmm
