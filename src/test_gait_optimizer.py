0007a4c3e1478414c1999bfb2a050cce852ab540
Stephen Eick <prinnydude97@gmail.com>
2019-02-14 12:21:32 -0500

    Initial attempt at Fourier Analysis functors

--- src/test_gait_optimizer.py
+++ src/test_gait_optimizer.py
@@ -8,11 +8,57 @@ Author: Frank Dellaert and Stephen Eick
 
 from __future__ import print_function
 
+from math import sin, cos
 import unittest
 
 import numpy as np
 
 from utils import GtsamTestCase
+
+
+class Basis(object):
+    """Caller for basis decompositions"""
+    
+    def __init__(self):
+        """Constructor"""
+        pass
+
+    def weightMatrix(self, x):
+        """Calculate the weight matrix for a given basis"""
+        return self.calculateWeights(x)
+
+
+class FourierBasis(Basis):
+    """ Class for Fourier analysis """
+
+    def __init__(self, N):
+        """ Constructor
+            Keyword arguments:
+                N -- number of values to evaluate
+        """
+        Basis.__init__(self)
+        self._N = N
+    
+    def calculateWeights(self, x):
+        """Evaluate Fourier weights at a given x"""
+        weights = []
+        weights.append(1)
+        for n in range(1, self._N / 2 + 1):
+            weights.append(sin(n * x))
+            weights.append(cos(n * x))
+        return np.array(weights)
+
+    class EvaluationFunctor(object):
+        
+        def __init__(self, N, x):
+            """Constructor"""
+            FourierBasis.__init__(N)
+            self._weights = self.weightMatrix(x)
+            
+        def __call__(self, parameters):
+            """Do Fourier synthesis at a given x"""
+            return self._weights * parameters
+
 
 
 class DynamicsModel(object):
@@ -75,6 +121,15 @@ class TestGaitOptimizer(GtsamTestCase):
         actual = self.optimizer.run(desired_velocity, stance_fraction)
         self.assertEqual(actual.shape, (7,))
 
+    def test_functors(self):
+        """Testing to make sure the functor system works as expected"""
+        numberOfParameters = 7
+        x = 1
+        basis = FourierBasis.EvaluationFunctor(numberOfParameters, x)
+        self.assertIsNotNone(basis)
+        val = basis(np.array([1, 2, 3]))
+        print(val)
+        
 
 if __name__ == "__main__":
     unittest.main()
