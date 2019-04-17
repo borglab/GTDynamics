90be29cb0a7498d77c4b1a1d77a461995ed03a40
yetongumich <yetong@umich.edu>
2019-04-17 13:50:07 -0400

    revise back adjointMap

--- src/test_urdf_link.py
+++ src/test_urdf_link.py
@@ -13,9 +13,11 @@ import unittest
 import utils
 from gtsam import GaussianFactorGraph, Point3, Pose3, Rot3, VectorValues
 from urdf_link import URDF_Link
+from serial_link import SerialLink
 from link import F, Link, T, a
 from utils import GtsamTestCase
 from urdf_parser_py.urdf import URDF
+import numpy as np
 
 ZERO1 = utils.vector(0)
 ZERO6 = utils.vector(0, 0, 0, 0, 0, 0)
@@ -40,8 +42,8 @@ class TestURDFLink(GtsamTestCase):
 
 
 def compose_pose(rpy, xyz):
-    # TODO: create the rotation
-    rotation = Rot3()
+    # TODO: check if the rotation is correct
+    rotation = Rot3.rpy(rpy[0], rpy[1], rpy[2])
     point = Point3(xyz[0], xyz[1], xyz[2])
     return Pose3(rotation, point)
 
@@ -64,17 +66,31 @@ class TestURDFFetch(GtsamTestCase):
                 if (link_name not in ordered_link_names) and robot.parent_map[link_name][1] in ordered_link_names:
                     ordered_link_names.append(link_name)
 
+        # TODO: build the links in a tree structure
+        # The serial link class only supports serial connection (one child policy)
+        # However, there may be several children connected to the parent in the fetch robot, and the links form a tree structure
+        # Currently, I only choose one path in the tree structure, and build a serial link
+
+        link_name = 'r_gripper_finger_link'  # leaf link
+        path_links = [link_name]
+        while link_name in robot.parent_map:
+            parent_name = robot.parent_map[link_name][1]
+            path_links = [parent_name] + path_links
+            link_name = parent_name
+
         # create each link with the urdf specs
-        self.links = {}
-        for link_name in ordered_link_names:
+        fetch_calibration_urdf = []
+        for link_name in path_links:
             link = link_dict[link_name]
 
             # ignore imaginary links
             if link.inertial is None:
                 continue
 
-            # TODO: include the whole inertia matrix (6 elements)
-            principal_inertia = [link.inertial.inertia.ixx, link.inertial.inertia.iyy, link.inertial.inertia.izz]
+            inertia = link.inertial.inertia
+            inertia_matrix = np.array(
+                [[inertia.ixx, inertia.ixy, inertia.ixz], [inertia.ixy, inertia.iyy, inertia.iyz], [inertia.ixz, inertia.iyz, inertia.izz]])
+
             mass = link.inertial.mass
             center_of_mass = compose_pose(link.inertial.origin.rpy, link.inertial.origin.xyz)
 
@@ -83,7 +99,7 @@ class TestURDFFetch(GtsamTestCase):
                 joint_name, parent_name = robot.parent_map[link_name]
                 joint = joint_dict[joint_name]
                 joint_type = 'R' if joint.joint_type in ['revolute', 'continuous'] else 'P'
-                # TODO: special case for fixed joint
+                # TODO: special case for fixed joint, combine both links as the same link, or set the joint angle to be fixed
                 axis = utils.vector(0, 0, 1) if joint.axis is None else utils.vector(joint.axis[0], joint.axis[1], joint.axis[2])
                 origin = compose_pose(joint.origin.rpy, joint.origin.xyz)
             else:  # base link
@@ -91,14 +107,12 @@ class TestURDFFetch(GtsamTestCase):
                 axis = utils.vector(0, 0, 1)
                 origin = compose_pose([0, 0, 0], [0, 0, 0])
 
-            # TODO: attach the link to its parent link
-
             # create the link
-            self.links[link_name] = URDF_Link(origin, axis, joint_type, mass, center_of_mass, principal_inertia)
+            fetch_calibration_urdf.append(URDF_Link(origin, axis, joint_type, mass, center_of_mass, inertia_matrix))
+            self.robot = SerialLink(fetch_calibration_urdf)
 
     def test_constructor(self):
-        for name, link in self.links.items():
-            self.assertIsInstance(link, URDF_Link)
+        self.assertIsInstance(self.robot, SerialLink)
 
 
 if __name__ == "__main__":
