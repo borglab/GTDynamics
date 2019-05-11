eceabf91f311b6f96b1f3c3e5cf3823a6df3528c
Frank Dellaert <dellaert@gmail.com>
2019-05-11 16:59:05 -0400

    6DOF solve code

--- src/frank02_ik.py
+++ src/frank02_ik.py
@@ -6,18 +6,18 @@ Requires sympy to be installed.
 
 import math
 import os
+import time
 import unittest
 
 import gtsam
 import numpy as np
 import sympy
-from sympy import Matrix, N, S, Symbol, solve_poly_system, solveset
+from sympy import Matrix, N, S, Symbol, pprint, solve_poly_system, solveset
 from sympy.solvers.solveset import nonlinsolve
 
 from serial_link import SerialLink
 from urdf_link import read_urdf
 from utils import GtsamTestCase, vector
-import time
 
 MY_PATH = os.path.dirname(os.path.realpath(__file__))
 URDFS_PATH = os.path.join(MY_PATH, '../urdfs')
@@ -33,37 +33,46 @@ class FasterIK:
         self._robot = SerialLink.from_urdf(link_dict, leaf_link_name="Part6")
 
     @staticmethod
-    def Z(label: str = ""):
+    def Z(label: str = "", trig=False):
         """ Create symbolic rotation matrix around Z-axis.
             Arguments:
                 label(str): symbols created are "cL" and "sL", where L is label.
+                trig (bool): whether to use cos/sin rather than c,s,c^2+s^2==1
             Returns:
                 (sympy.Matrix, unit constraint on "cL" and "sL")
         """
-        c = Symbol("c"+label, real=True)
-        s = Symbol("s"+label, real=True)
-        return Matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]]), c**2 + s**2 - 1, [c, s]
+        if trig:
+            theta = Symbol("\\theta"+label, real=True)
+            c, s = sympy.cos(theta), sympy.sin(theta)
+            R = Matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
+            return R, [], [theta]
+        else:
+            c = Symbol("c"+label, real=True)
+            s = Symbol("s"+label, real=True)
+            R = Matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
+            return R, c**2 + s**2 - 1, [c, s]
 
     @classmethod
-    def RRR_rotation(cls, R12, R23, R3e, label: str = ""):
-        """ Calculate symbolic expression of the rotation matrix at the end of three links. 
+    def RRR_rotation(cls, R12, R23, R3e, label: str = "", trig=False):
+        """ Calculate symbolic expression of the rotation matrix at the end of three links.
             Arguments:
                 Rij: rotation matrices from frame i to frame j.
                      Frame 1 corresponding to joint angle 1 is assumed identity.
                      The end-effector frame E is related to frame 3 by R3e.
-                label(str): symbols created are "cL1", "cL2" etc... where L is the label.
+                label (str): symbols created are "cL1", "cL2" etc... where L is the label.
+                trig (bool): whether to use cos/sin rather than c,s,c^2+s^2==1
             Returns:
                 rotation, [Z-matrices], [unit constraints for the angles], [symbols]
         """
-        Z1, e1, syms1 = cls.Z(label+"1")
-        Z2, e2, syms2 = cls.Z(label+"2")
-        Z3, e3, syms3 = cls.Z(label+"3")
+        Z1, e1, syms1 = cls.Z(label+"1", trig)
+        Z2, e2, syms2 = cls.Z(label+"2", trig)
+        Z3, e3, syms3 = cls.Z(label+"3", trig)
         syms = syms1 + syms2 + syms3
         return Z1 * R12 * Z2 * R23 * Z3 * R3e, [Z1, Z2, Z3], [e1, e2, e3], syms
 
     @classmethod
-    def RRR_pose(cls, R12, t12, R23, t23, R3e, t3e, label: str = ""):
-        """ Calculate symbolic expression of the pose at the end of three links. 
+    def RRR_pose(cls, R12, t12, R23, t23, R3e, t3e, label: str = "", trig=False):
+        """ Calculate symbolic expression of the pose at the end of three links.
             Arguments:
                 Tij: link transform from frame i to frame j.
                      Frame 1 corresponding to joint angle 1 is assumed identity.
@@ -72,14 +81,29 @@ class FasterIK:
             Returns:
                 rotation, translation, [unit constraints for the angles], [symbols]
         """
-        Re, Zs, constraints, syms = cls.RRR_rotation(R12, R23, R3e, label)
+        Re, Zs, constraints, syms = cls.RRR_rotation(
+            R12, R23, R3e, label, trig)
         Z1, Z2, Z3 = Zs
         te = Z1*(t12 + R12*Z2*(t23 + R23*Z3*t3e))
         return Re, te, Zs, constraints, syms
 
     @classmethod
+    def R6_pose(cls, *robot):
+        """ Calculate symbolic expression of the pose at the end of 6DOF manipulator.
+            Arguments:
+                robot: list of [R, t, R, t...]
+            Returns:
+                rotation, translation, [unit constraints for the angles], [symbols]
+        """
+        R4, t4, ZA, constraintsA, symsA = cls.RRR_pose(*robot[:6], "a")
+        R4e, t4e, ZB, constraintsB, symsB = cls.RRR_pose(*robot[6:], "b")
+        Re = R4 * R4e
+        te = t4 + R4 * t4e
+        return Re, te, ZA+ZB, constraintsA+constraintsB, symsA+symsB
+
+    @classmethod
     def RRR_solve(cls, R12, t12, R23, t23, R3e, t3e, lock_theta1=False, label: str = ""):
-        """ Solve for RRR link, as a function of position (x,0,z). 
+        """ Solve for RRR link, as a function of position (x,0,z).
             The idea is that for non-zero y the first joint can be rotated appropriately.
             Typically returns two solutions, corresponding to two elbow configurations.
         """
@@ -95,9 +119,28 @@ class FasterIK:
             # This *also* disallows backward reaching from theta1=180
             c1, s1 = syms[0:2]
             syms = syms[2:]
-            constraints = [c.subs([(c1, 1), (s1, 0)]) for c in constraints[1:]]
+            constraints = constraints[1:]
             te = te.subs([(c1, 1), (s1, 0)])
         equations = sympy.flatten(te-td) + constraints
+        return solve_poly_system(equations, syms), syms + [x, z]
+
+    @classmethod
+    def R6_solve(cls, lock_theta1, *robot):
+        """ Solve for 6DOF arm, as a function of position (x,0,z).
+        """
+        Re, te, Zs, constraints, syms = cls.R6_pose(*robot)
+        x, z = sympy.symbols('x z')
+        Rd = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        td = Matrix([[x], [0], [z]], real=True)
+        if lock_theta1:
+            # Same as in RRR_solve
+            c1, s1 = syms[0:2]
+            syms = syms[2:]
+            constraints = constraints[1:]
+            te = te.subs([(c1, 1), (s1, 0)])
+        equations = sympy.flatten(Re-Rd) + sympy.flatten(te-td) + constraints
+        # equations = [sympy.expand(e) for e in equations]
+        print(equations)
         return solve_poly_system(equations, syms), syms + [x, z]
 
     @staticmethod
@@ -107,7 +150,7 @@ class FasterIK:
         for s in solutions:
             print("---------------------------")
             for si in sympy.cse(sympy.simplify(s)):
-                sympy.pprint(si)
+                pprint(si)
 
     @staticmethod
     def print_solutions_for(solutions, *substitutions):
@@ -168,8 +211,25 @@ class TestInverseKinematics(GtsamTestCase):
         self.assertEqual(len(constraints), 3)
         self.assertEqual(len(syms), 6)
 
+    def test_R6_pose(self):
+        """Check expression for pose at end of 6DOF manipulator."""
+        robot = 2*self.ROBOT
+        Re, te, Zs, constraints, syms = FasterIK.R6_pose(*robot)
+        self.assertIsInstance(Re, sympy.Matrix)
+        self.assertIsInstance(constraints, list)
+        self.assertEqual(len(constraints), 6)
+        self.assertEqual(len(syms), 12)
+
+    @unittest.skip("too slow")
     def test_RRR_solve(self):
         solutions, syms = FasterIK.RRR_solve(*self.ROBOT, lock_theta1=True)
+        self.assertEqual(len(solutions), 2)
+        self.assertEqual(len(syms), 6)
+
+    @unittest.skip("too slow")
+    def test_R6_solve(self):
+        robot = 2*self.ROBOT
+        solutions, syms = FasterIK.R6_solve(True, *robot)
         self.assertEqual(len(solutions), 2)
         self.assertEqual(len(syms), 6)
 
@@ -186,14 +246,43 @@ class TestInverseKinematics(GtsamTestCase):
         # np.testing.assert_array_almost_equal(theta_A, vector(1, 2, 3))
 
 
-if __name__ == "__main__":
-    start = time.time()
-    solutions, syms = FasterIK.RRR_solve(
-        *TestInverseKinematics.ROBOT, lock_theta1=True)
-    end = time.time()
-    print("elapsed time = {} secs", end - start)
+def timeit(method):
+    """https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d"""
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
+
+@timeit
+def run_rrr():
+    """Run RRR solver for simple example."""
+    robot = TestInverseKinematics.ROBOT
+    solutions, syms = FasterIK.RRR_solve(*robot, lock_theta1=True)
     FasterIK.print_solutions(solutions)
     x, z = syms[-2:]
     FasterIK.print_solutions_for(solutions, (x, 1.5), (z, 2))
+
+
+@timeit
+def run_r6():
+    """Run 6DOF solver for simple example."""
+    robot = 2*TestInverseKinematics.ROBOT
+    lock_theta1 = False
+    solutions, syms = FasterIK.R6_solve(lock_theta1, *robot)
+    FasterIK.print_solutions(solutions)
+    x, z = syms[-2:]
+    FasterIK.print_solutions_for(solutions, (x, 1.5), (z, 2))
+
+
+if __name__ == "__main__":
+    run_rrr()
+    # run_r6()
     unittest.main()
