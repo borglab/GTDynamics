5b23090cc4639efed851c65be6c060afbdce0936
Gerry Chen <gdc9@duke.edu>
2020-03-13 15:38:41 -0400

    style guide: 2 space indents

--- scripts/gerry00_ForwardDynamicsPrismatic.cpp
+++ scripts/gerry00_ForwardDynamicsPrismatic.cpp
@@ -18,60 +18,59 @@
 #include <gtdynamics/dynamics/DynamicsGraph.h>
 
 int main(int argc, char** argv) {
-    // Load the robot and build a nonlinear factor graph of kinodynamics
-    // constraints.
-    auto simple_rpr = gtdynamics::Robot(
-        "../../sdfs/test/simple_rpr.sdf", "simple_rpr_sdf");
-    std::cout << "\033[1;31m" << "Robot Model:" << "\033[0m\n" << std::endl;
-    simple_rpr.printRobot();
+  // Load the robot and build a nonlinear factor graph of kinodynamics
+  // constraints.
+  auto simple_rpr = gtdynamics::Robot(
+    "../../sdfs/test/simple_rpr.sdf", "simple_rpr_sdf");
+  std::cout << "\033[1;31m" << "Robot Model:" << "\033[0m\n" << std::endl;
+  simple_rpr.printRobot();
 
-    // Simulation parameters
-    int T = 15;
-    const double dt = 0.1;
+  // Simulation parameters
+  int T = 15;
+  const double dt = 0.1;
 
-    // Build graph
-    auto graph_builder = gtdynamics::DynamicsGraph();
-    gtsam::Vector3 gravity = (gtsam::Vector(3) << 0, 0, 0).finished();
-    auto kdfg = graph_builder.trajectoryFG(
-        simple_rpr,
-        T,
-        dt,
-        gtdynamics::DynamicsGraph::CollocationScheme::Euler,
-        gravity);
+  // Build graph
+  auto graph_builder = gtdynamics::DynamicsGraph();
+  gtsam::Vector3 gravity = (gtsam::Vector(3) << 0, 0, 0).finished();
+  auto kdfg = graph_builder.trajectoryFG(
+      simple_rpr,
+      T,
+      dt,
+      gtdynamics::DynamicsGraph::CollocationScheme::Euler,
+      gravity);
 
-    // Specify the forward dynamics priors and add them to the factor graph.
-    gtsam::Vector theta = (gtsam::Vector(3) << 0, 0, 0).finished();
-    gtsam::Vector theta_dot = (gtsam::Vector(3) << 0.3, 0.1, 0).finished();
-    std::vector<gtsam::Vector> taus;
-    for (int t = 0; t <= T; t++) {
-        taus.push_back((gtsam::Vector(3) << 0, 0, 0).finished());
-    }
-    auto fd_priors = graph_builder.trajectoryFDPriors(simple_rpr, T,
-        theta, theta_dot, taus);
-    kdfg.add(fd_priors);
+  // Specify the forward dynamics priors and add them to the factor graph.
+  gtsam::Vector theta = (gtsam::Vector(3) << 0, 0, 0).finished();
+  gtsam::Vector theta_dot = (gtsam::Vector(3) << 0.3, 0.1, 0).finished();
+  std::vector<gtsam::Vector> taus;
+  for (int t = 0; t <= T; t++) {
+    taus.push_back((gtsam::Vector(3) << 0, 0, 0).finished());
+  }
+  auto fd_priors = graph_builder.trajectoryFDPriors(simple_rpr, T, theta, theta_dot, taus);
+  kdfg.add(fd_priors);
 
-    // Initialize solution.
-    auto init_values = graph_builder.zeroValuesTrajectory(simple_rpr, T, 0);
+  // Initialize solution.
+  auto init_values = graph_builder.zeroValuesTrajectory(simple_rpr, T, 0);
 
-    // Compute the forward dynamics.
-    gtsam::LevenbergMarquardtOptimizer optimizer(kdfg, init_values);
-    gtsam::Values results = optimizer.optimize();
-    // graph_builder.printValues(results);
-    std::cout << "\033[1;31m" << "Joint Angles:" << "\033[0m" << std::endl;
-    for (int t = 0; t <= T; t++) {
-        std::cout << graph_builder.jointAngles(simple_rpr, results, t) << std::endl << std::endl;
-    }
-    std::cout << "\033[1;31m" << "Link Poses:" << "\033[0m\n" << std::endl;
-    for (int t = 0; t <= T; t++) {
-        results.at<gtsam::Pose3>(gtdynamics::PoseKey(0, t)).translation().print();
-        results.at<gtsam::Pose3>(gtdynamics::PoseKey(1, t)).translation().print();
-        results.at<gtsam::Pose3>(gtdynamics::PoseKey(2, t)).translation().print();
-        results.at<gtsam::Pose3>(gtdynamics::PoseKey(3, t)).translation().print();
-        std::cout << std::endl;
-    }
+  // Compute the forward dynamics.
+  gtsam::LevenbergMarquardtOptimizer optimizer(kdfg, init_values);
+  gtsam::Values results = optimizer.optimize();
+  // graph_builder.printValues(results);
+  std::cout << "\033[1;31m" << "Joint Angles:" << "\033[0m" << std::endl;
+  for (int t = 0; t <= T; t++) {
+    std::cout << graph_builder.jointAngles(simple_rpr, results, t) << std::endl << std::endl;
+  }
+  std::cout << "\033[1;31m" << "Link Poses:" << "\033[0m\n" << std::endl;
+  for (int t = 0; t <= T; t++) {
+    results.at<gtsam::Pose3>(gtdynamics::PoseKey(0, t)).translation().print();
+    results.at<gtsam::Pose3>(gtdynamics::PoseKey(1, t)).translation().print();
+    results.at<gtsam::Pose3>(gtdynamics::PoseKey(2, t)).translation().print();
+    results.at<gtsam::Pose3>(gtdynamics::PoseKey(3, t)).translation().print();
+    std::cout << std::endl;
+  }
 
-    // export
-    graph_builder.saveGraphMultiSteps("gerry00_result.json", kdfg, results, simple_rpr, T);
+  // export
+  graph_builder.saveGraphMultiSteps("gerry00_result.json", kdfg, results, simple_rpr, T);
 
-    return 0;
+  return 0;
 }
