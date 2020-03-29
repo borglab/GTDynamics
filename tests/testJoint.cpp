233fdd0a70a2e74698168bd14f0c226981bf964f
Gerry Chen <gdc9@duke.edu>
2020-03-29 01:44:43 -0400

    fix test sdf files: * jumping_robot.sdf - fixes needed.  model validated visually in gazebo * simple_rr_2.sdf - a model identical to simple_rr.sdf except with different link coordinate frames to create a failing test case

--- tests/testJoint.cpp
+++ tests/testJoint.cpp
@@ -368,9 +368,82 @@ TEST(Joint, params_constructor_invalid) {
   THROWS_EXCEPTION(JointSharedPtr j1 = std::make_shared<Joint>(Joint(params)));
 }
 
+/**
+ * Construct the same joint via sdf and ensure all values are as expected.
+ */
 TEST(Joint, sdf_constructor) {
   auto model =
       get_sdf(std::string(SDF_PATH) + "/test/simple_rr.sdf", "simple_rr_sdf");
+
+  LinkSharedPtr l0 = std::make_shared<Link>(Link(*model.LinkByName("link_0")));
+  LinkSharedPtr l1 = std::make_shared<Link>(Link(*model.LinkByName("link_1")));
+  LinkSharedPtr l2 = std::make_shared<Link>(Link(*model.LinkByName("link_2")));
+
+  // constructor for j1
+  gtdynamics::JointParams j1_params;
+  j1_params.name = "j1";
+  j1_params.jointEffortType = gtdynamics::Joint::JointEffortType::Actuated;
+  JointSharedPtr j1 = std::make_shared<Joint>(
+      Joint(*model.JointByName("joint_1"), j1_params.jointEffortType,
+            j1_params.springCoefficient, j1_params.jointLimitThreshold,
+            j1_params.velocityLimitThreshold, j1_params.accelerationLimit,
+            j1_params.accelerationLimitThreshold,
+            j1_params.torqueLimitThreshold, l0, l1));
+
+  // check screw axis
+  gtsam::Vector6 screw_axis_j1_l0, screw_axis_j1_l1;
+  screw_axis_j1_l0 << 0, 0, -1, 0, 0, 0;
+  screw_axis_j1_l1 << 0, 0, 1, 0, 0, 0;
+  EXPECT(assert_equal(screw_axis_j1_l0, j1->screwAxis(l0)));
+  EXPECT(assert_equal(screw_axis_j1_l1, j1->screwAxis(l1)));
+
+  // Check transform from l0 com to l1 com at rest and at various angles.
+  gtsam::Pose3 T_01comRest(gtsam::Rot3::identity(), gtsam::Point3(0, 0, 0.4));
+  gtsam::Pose3 T_01com_neg(gtsam::Rot3::Rz(-M_PI / 2),
+                           gtsam::Point3(0, 0, 0.4));
+  gtsam::Pose3 T_01com_pos(gtsam::Rot3::Rz(M_PI / 2), gtsam::Point3(0, 0, 0.4));
+
+  EXPECT(assert_equal(T_01comRest, j1->transformTo(l0)));
+  EXPECT(assert_equal(T_01com_neg, j1->transformTo(l0, -M_PI / 2)));
+  EXPECT(assert_equal(T_01com_pos, j1->transformFrom(l1, M_PI / 2)));
+
+  // constructor for j2
+  gtdynamics::JointParams j2_params;
+  j2_params.name = "j2";
+  j2_params.jointEffortType = gtdynamics::Joint::JointEffortType::Actuated;
+  JointSharedPtr j2 = std::make_shared<Joint>(
+      Joint(*model.JointByName("joint_2"), j2_params.jointEffortType,
+            j2_params.springCoefficient, j2_params.jointLimitThreshold,
+            j2_params.velocityLimitThreshold, j2_params.accelerationLimit,
+            j2_params.accelerationLimitThreshold,
+            j2_params.torqueLimitThreshold, l1, l2));
+
+  // check screw axis
+  gtsam::Vector6 screw_axis_j2_l1, screw_axis_j2_l2;
+  screw_axis_j2_l1 << 0, -1, 0, 0.3, 0, 0;
+  screw_axis_j2_l2 << 0, 1, 0, 0.3, 0, 0;
+  EXPECT(assert_equal(screw_axis_j2_l1, j2->screwAxis(l1)));
+  EXPECT(assert_equal(screw_axis_j2_l2, j2->screwAxis(l2)));
+
+  // Check transform from l1 com to l2 com at rest and at various angles.
+  gtsam::Pose3 T_12com_rest(gtsam::Rot3::identity(), gtsam::Point3(0, 0, 0.6));
+  gtsam::Pose3 T_12com_pi_2(gtsam::Rot3::Ry(M_PI / 2),
+                            gtsam::Point3(0.3, 0.0, 0.3));
+  gtsam::Pose3 T_12com_pi_4(gtsam::Rot3::Ry(M_PI / 4),
+                            gtsam::Point3(0.2121, 0.0, 0.5121));
+
+  EXPECT(assert_equal(T_12com_rest, j2->transformFrom(l2)));
+  EXPECT(assert_equal(T_12com_pi_2, j2->transformFrom(l2, M_PI / 2.0)));
+  EXPECT(assert_equal(T_12com_pi_4, j2->transformTo(l1, M_PI / 4.0), 1e-3));
+}
+
+/**
+ * Construct the same joint via a different sdf formulation and ensure all
+ * values are as expected.
+ */
+TEST(Joint, sdf_constructor2) {
+  auto model =
+      get_sdf(std::string(SDF_PATH) + "/test/simple_rr_2.sdf", "simple_rr_sdf");
 
   LinkSharedPtr l0 = std::make_shared<Link>(Link(*model.LinkByName("link_0")));
   LinkSharedPtr l1 = std::make_shared<Link>(Link(*model.LinkByName("link_1")));
