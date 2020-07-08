5193886e7a855c6adb3f3ab1b42f8a006a2864a9
Stephanie McCormick <step13489mcc@gmail.com>
2020-07-08 18:06:44 -0400

    - using joint constructors instead of shared ptrs

--- tests/testScrewJoint.cpp
+++ tests/testScrewJoint.cpp
@@ -36,64 +36,63 @@ TEST(Joint, params_constructor) {
       std::make_shared<Link>(*simple_urdf.LinkByName("l2"));
 
   Joint::Parameters parameters;
-  parameters.effort_type = Joint::JointEffortType::Actuated;
+  parameters.effort_type = Joint::EffortType::Actuated;
   parameters.scalar_limits.value_lower_limit = -1.57;
   parameters.scalar_limits.value_upper_limit = 1.57;
   parameters.scalar_limits.value_limit_threshold = 0;
 
-  ScrewJointSharedPtr j1 =
-      std::make_shared<ScrewJoint>("j1", Pose3(Rot3(), Point3(0, 0, 2)), l1, l2,
-                                   parameters, gtsam::Vector3(1, 0, 0), 0.5);
+  ScrewJoint j1("j1", Pose3(Rot3(), Point3(0, 0, 2)), l1, l2, parameters,
+                gtsam::Vector3(1, 0, 0), 0.5);
 
   // name
-  EXPECT(assert_equal(j1->name(), "j1"));
+  EXPECT(assert_equal(j1.name(), "j1"));
 
   // joint type
-  EXPECT(j1->type() == Joint::Type::Screw);
+  EXPECT(j1.type() == Joint::Type::Screw);
 
   // joint effort type
-  EXPECT(j1->parameters().effort_type == Joint::JointEffortType::Actuated);
+  EXPECT(j1.parameters().effort_type == Joint::EffortType::Actuated);
 
   // other link
-  EXPECT(j1->otherLink(l2) == l1);
-  EXPECT(j1->otherLink(l1) == l2);
+  EXPECT(j1.otherLink(l2) == l1);
+  EXPECT(j1.otherLink(l1) == l2);
 
   // screw axis
   gtsam::Vector6 screw_axis_l1, screw_axis_l2;
   screw_axis_l1 << -1, 0, 0, -0.5 / 2 / M_PI, -1, 0;  // parent frame
   screw_axis_l2 << 1, 0, 0, 0.5 / 2 / M_PI, -1, 0;    // child frame
-  EXPECT(assert_equal(screw_axis_l2, j1->screwAxis(l2)));
-  EXPECT(assert_equal(screw_axis_l1, j1->screwAxis(l1)));
+  EXPECT(assert_equal(screw_axis_l2, j1.screwAxis(l2)));
+  EXPECT(assert_equal(screw_axis_l1, j1.screwAxis(l1)));
 
   // rest transform
   Pose3 T_12comRest(Rot3::Rx(0), Point3(0, 0, 2));
   Pose3 T_21comRest(Rot3::Rx(0), Point3(0, 0, -2));
-  EXPECT(assert_equal(T_12comRest, j1->transformFrom(l2)));
-  EXPECT(assert_equal(T_21comRest, j1->transformTo(l2)));
+  EXPECT(assert_equal(T_12comRest, j1.transformFrom(l2)));
+  EXPECT(assert_equal(T_21comRest, j1.transformTo(l2)));
 
   // transform from (rotating -pi/2)
   Pose3 T_12com(Rot3::Rx(-M_PI / 2), Point3(-0.125, 1, 1));
   Pose3 T_21com(Rot3::Rx(M_PI / 2), Point3(0.125, 1, -1));
-  EXPECT(assert_equal(T_12com, j1->transformFrom(l2, -M_PI / 2)));
-  EXPECT(assert_equal(T_21com, j1->transformFrom(l1, -M_PI / 2)));
+  EXPECT(assert_equal(T_12com, j1.transformFrom(l2, -M_PI / 2)));
+  EXPECT(assert_equal(T_21com, j1.transformFrom(l1, -M_PI / 2)));
 
   // transfrom to (rotating -pi/2)
-  EXPECT(assert_equal(T_12com, j1->transformTo(l1, -M_PI / 2)));
-  EXPECT(assert_equal(T_21com, j1->transformTo(l2, -M_PI / 2)));
+  EXPECT(assert_equal(T_12com, j1.transformTo(l1, -M_PI / 2)));
+  EXPECT(assert_equal(T_21com, j1.transformTo(l2, -M_PI / 2)));
 
   // links
-  auto links = j1->links();
+  auto links = j1.links();
   EXPECT(links[0] == l1);
   EXPECT(links[1] == l2);
 
   // parent & child link
-  EXPECT(j1->parentLink() == l1);
-  EXPECT(j1->childLink() == l2);
+  EXPECT(j1.parentLink() == l1);
+  EXPECT(j1.childLink() == l2);
 
   // joint limit
-  EXPECT(assert_equal(parameters.scalar_limits.value_lower_limit, j1->parameters().scalar_limits.value_lower_limit));
-  EXPECT(assert_equal(parameters.scalar_limits.value_upper_limit, j1->parameters().scalar_limits.value_upper_limit));
-  EXPECT(assert_equal(parameters.scalar_limits.value_limit_threshold, j1->parameters().scalar_limits.value_limit_threshold));
+  EXPECT(assert_equal(parameters.scalar_limits.value_lower_limit, j1.parameters().scalar_limits.value_lower_limit));
+  EXPECT(assert_equal(parameters.scalar_limits.value_upper_limit, j1.parameters().scalar_limits.value_upper_limit));
+  EXPECT(assert_equal(parameters.scalar_limits.value_limit_threshold, j1.parameters().scalar_limits.value_limit_threshold));
 }
 
 int main() {
