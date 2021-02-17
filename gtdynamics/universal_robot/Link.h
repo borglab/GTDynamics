278da46529ea4241f814d1aa9f22af9d8ae52144
Varun Agrawal <varagrawal@gmail.com>
2021-02-17 12:57:47 -0500

    Added equality operators and moved some functions to public space

--- gtdynamics/universal_robot/Link.h
+++ gtdynamics/universal_robot/Link.h
@@ -28,9 +28,9 @@
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/slam/PriorFactor.h>
 
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
 #include <memory>
 #include <string>
 #include <vector>
