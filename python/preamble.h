0cb63512c4ea23815ca619a1dbbd9b36f8364df6
Varun Agrawal <varagrawal@gmail.com>
2021-03-15 23:18:00 -0400

    updated preamble and specializations, added potential binding code

--- python/preamble.h
+++ python/preamble.h
@@ -3,5 +3,9 @@ namespace pybind11 { namespace detail {
     struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
 }}
 
-PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::JointSharedPtr>);
-PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::LinkSharedPtr>);
+/// Needed to ensure there is reference passing
+/// https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#making-opaque-types
+// PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::JointSharedPtr>);
+// PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::LinkSharedPtr>);
+// PYBIND11_MAKE_OPAQUE(std::map<std::string, gtdynamics::JointSharedPtr>);
+// PYBIND11_MAKE_OPAQUE(std::map<std::string, gtdynamics::LinkSharedPtr>);
