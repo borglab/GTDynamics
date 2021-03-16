bdd2316da28039a6932e4c562256a87c5e36883d
Varun Agrawal <varagrawal@gmail.com>
2021-03-16 18:14:43 -0400

    remove commented out pybind11 macros

--- python/preamble.h
+++ python/preamble.h
@@ -2,10 +2,3 @@ namespace pybind11 { namespace detail {
     template <typename T>
     struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
 }}
-
-/// Needed to ensure there is reference passing
-/// https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#making-opaque-types
-// PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::JointSharedPtr>);
-// PYBIND11_MAKE_OPAQUE(std::vector<gtdynamics::LinkSharedPtr>);
-// PYBIND11_MAKE_OPAQUE(std::map<std::string, gtdynamics::JointSharedPtr>);
-// PYBIND11_MAKE_OPAQUE(std::map<std::string, gtdynamics::LinkSharedPtr>);
--- python/specializations.h
+++
@@ -1,5 +0,0 @@
-/// Also needs to be done if using PYBIND11_MAKE_OPAQUE
-// py::bind_vector<std::vector<gtdynamics::JointSharedPtr> >(m_, "JointVector");
-// py::bind_vector<std::vector<gtdynamics::LinkSharedPtr> >(m_, "LinkVector");
-// py::bind_map<std::map<std::string, gtdynamics::JointSharedPtr> >(m_, "JointMap");
-// py::bind_map<std::map<std::string, gtdynamics::LinkSharedPtr> >(m_, "LinkMap");
