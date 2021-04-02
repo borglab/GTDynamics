bef201a37ceef3a6145d17586a7509e85542c323
Gerry Chen <gerry.chen2015@gmail.com>
2021-04-02 03:26:45 -0400

    update new cable vel factor

--- gtdynamics/cablerobot/tests/testCableVelFactor.cpp
+++ gtdynamics/cablerobot/tests/testCableVelFactor.cpp
@@ -1,10 +1,13 @@
 /**
  * @file  testCableVelFactor.cpp
  * @brief test cable vel factor
- * @Author: Frank Dellaert and Gerry Chen
+ * @author Frank Dellaert
+ * @author Gerry Chen
  */
 
 #include "factors/CableVelFactor.h"
+
+#include <gtdynamics/utils/values.h>
 
 #include <gtsam/base/Vector.h>
 #include <gtsam/inference/Symbol.h>
@@ -18,7 +21,8 @@
 
 using namespace std;
 using namespace gtsam;
-using namespace gtdynamics::cablerobot;
+using namespace gtdynamics;
+using namespace gtdynamics::internal;
 
 /**
  * Test cable factor
@@ -28,69 +32,38 @@ TEST(CableVelFactor, error) {
   noiseModel::Gaussian::shared_ptr cost_model =
       noiseModel::Isotropic::Sigma(1, 1.0);
 
-  Symbol points[2] = {symbol('p', 0), symbol('p', 1)};
-  Symbol vels[2] = {symbol('f', 0), symbol('f', 1)};
-  Symbol v = symbol('t', 0);
-  CableVelFactor<Point3> factor(v, points[0], points[1], vels[0], vels[1],
-                                cost_model);
+  int jid = 0;
+  int lid = 0;
+  Point3 frameLoc = Point3(0.1, 0.2, 0.3);
+  Point3 eeLoc = Point3(-0.15, 0, -0.15);
 
-  double conf_v = -2;
-  Point3 conf_points[2] = {Point3(0, 0, 0), Point3(1, 0, 0)};
-  Vector conf_vels[2] = {Vector3(1, 0, 0), Vector3(-1, 0, 0)};
-  Vector1 expected_errors { 0 };
+  CableVelFactor factor(JointVelKey(jid), PoseKey(lid), TwistKey(lid),
+                        cost_model, frameLoc, eeLoc);
 
+  Values values;
+  InsertJointVel(&values, jid, 1.0);
+  InsertPose(&values, lid, Pose3(Rot3(), Point3(1.5, 0, 1.5)));
+  InsertTwist(&values, lid, (Vector6() << 0, 1, 0, 1.1, 0, 1.2).finished());
+
+  // "expected" calculation
+  Point3 dir = normalize(Point3(1, -0.2, 0.9));
+  Vector3 expected_v = Point3(1.1 - 0.15, 0, 1.2 + 0.15);
+  double expected_l = dot(dir, expected_v);
+  Vector1 expected_errors{expected_l - JointVel(values, jid)};
+
+  // evaluateError
   Vector1 actual_errors = factor.evaluateError(
-    conf_v, conf_points[0], conf_points[1], conf_vels[0], conf_vels[1]);
+      JointVel(values, jid), Pose(values, lid), Twist(values, lid));
 
   EXPECT(assert_equal(expected_errors, actual_errors, 1e-4));
 
-  Values values;
-  values.insertDouble(v, conf_v);
-  values.insert(points[0], conf_points[0]);
-  values.insert(points[1], conf_points[1]);
-  values.insert(vels[0], conf_vels[0]);
-  values.insert(vels[1], conf_vels[1]);
+  // jacobians
   EXPECT_CORRECT_FACTOR_JACOBIANS(factor, values, 1e-7, 1e-3);
 
-  values.update(points[1], Point3(3, 5, 7));
-  values.update(vels[1], static_cast<gtsam::Vector>(Vector3(-13, 17, 19)));
-  EXPECT_CORRECT_FACTOR_JACOBIANS(factor, values, 1e-7, 1e-3);
-}
-
-/**
- * Test cable factor, in Point2
- */
-TEST(CableVelFactor, errorPoint2) {
-  // noise model
-  noiseModel::Gaussian::shared_ptr cost_model =
-      noiseModel::Isotropic::Sigma(1, 1.0);
-
-  Symbol points[2] = {symbol('p', 0), symbol('p', 1)};
-  Symbol vels[2] = {symbol('f', 0), symbol('f', 1)};
-  Symbol v = symbol('t', 0);
-  CableVelFactor<Point2> factor(v, points[0], points[1], vels[0], vels[1],
-                                cost_model);
-
-  double conf_v = -5;
-  Point2 conf_points[2] = {Point2(5, 5), Point2(3, 7)};
-  Vector conf_vels[2] = {Vector2(0, 0), Vector2(5 * 1.4142135624, 0)};
-  Vector1 expected_errors { 0 };
-
-  Vector1 actual_errors = factor.evaluateError(
-    conf_v, conf_points[0], conf_points[1], conf_vels[0], conf_vels[1]);
-
-  EXPECT(assert_equal(expected_errors, actual_errors, 1e-4));
-
-  Values values;
-  values.insertDouble(v, conf_v);
-  values.insert(points[0], conf_points[0]);
-  values.insert(points[1], conf_points[1]);
-  values.insert(vels[0], conf_vels[0]);
-  values.insert(vels[1], conf_vels[1]);
-  EXPECT_CORRECT_FACTOR_JACOBIANS(factor, values, 1e-7, 1e-3);
-
-  values.update(points[1], Point2(3, 7));
-  values.update(vels[1], static_cast<gtsam::Vector>(Vector2(-13, 19)));
+  values.update(PoseKey(lid),
+                Pose3(Rot3::Rz(0.4), Point3(0.2, 0.3, 0.4)));
+  values.update(TwistKey(lid),
+                (Vector6() << 0.12, 0.13, 0.14, 0.18, 0.19, 0.21).finished());
   EXPECT_CORRECT_FACTOR_JACOBIANS(factor, values, 1e-7, 1e-3);
 }
 
