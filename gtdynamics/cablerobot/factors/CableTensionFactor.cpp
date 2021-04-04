600ccd70790e850c7873ca29a1677ea3d5545f92
Gerry Chen <gerry.chen2015@gmail.com>
2021-04-04 02:30:05 -0400

    formatting on CableTensionFactor.cpp

--- gtdynamics/cablerobot/factors/CableTensionFactor.cpp
+++ gtdynamics/cablerobot/factors/CableTensionFactor.cpp
@@ -6,7 +6,7 @@
  * @author Gerry Chen
  */
 
-#include "gtdynamics/cablerobot/factors/CableTensionFactor.h"
+#include "CableTensionFactor.h"
 
 #include <gtdynamics/utils/DynamicsSymbol.h>
 
@@ -16,7 +16,6 @@
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
 #include <boost/optional.hpp>
-
 #include <iostream>
 #include <string>
 
@@ -26,36 +25,35 @@ namespace gtdynamics {
 
 /******************************************************************************/
 Vector6 CableTensionFactor::computeWrench(
-    double t, const Pose3 &wTee,
-    boost::optional<gtsam::Matrix &> H_t,
-    boost::optional<gtsam::Matrix &> H_wTee) const {
+    double t, const Pose3 &wTee, boost::optional<Matrix &> H_t,
+    boost::optional<Matrix &> H_wTee) const {
   // Jacobians: cable direction
-  gtsam::Matrix33 dir_H_wPem;
-  gtsam::Matrix36 wPem_H_wTee;
+  Matrix33 dir_H_wPem;
+  Matrix36 wPem_H_wTee;
   // Jacobians: force to wrench conversion
-  gtsam::Matrix31 wf_H_t;
-  gtsam::Matrix33 wf_H_dir;
-  gtsam::Matrix33 eef_H_wf;
-  gtsam::Matrix33 eef_H_wRee;
-  gtsam::Matrix63 H_eef;
-  gtsam::Matrix33 eem_H_eef;  // = H_eef.topRows<3>(); TODO(gerry): pointer?
+  Matrix31 wf_H_t;
+  Matrix33 wf_H_dir;
+  Matrix33 eef_H_wf;
+  Matrix33 eef_H_wRee;
+  Matrix63 H_eef;
+  Matrix33 eem_H_eef;  // = H_eef.topRows<3>(); TODO(gerry): pointer?
 
   // cable direction
   Point3 wPem = wTee.transformFrom(eePem_, H_wTee ? &wPem_H_wTee : 0);
-  Vector3 dir = gtsam::normalize(wPem - wPb_, H_wTee ? &dir_H_wPem : 0);
+  Vector3 dir = normalize(wPem - wPb_, H_wTee ? &dir_H_wPem : 0);
   // force->wrench
   Vector3 wf = -t * dir;
   if (H_t) wf_H_t = -dir;
-  if (H_wTee) wf_H_dir = -t * gtsam::I_3x3;
+  if (H_wTee) wf_H_dir = -t * I_3x3;
   Vector3 eef = wTee.rotation().unrotate(wf,  //
                                          H_wTee ? &eef_H_wRee : 0,
                                          (H_t || H_wTee) ? &eef_H_wf : 0);
-  Vector3 eem = gtsam::cross(eePem_, eef,  //
-                             boost::none,  //
-                             (H_t || H_wTee) ? &eem_H_eef : 0);
+  Vector3 eem = cross(eePem_, eef,  //
+                      boost::none,  //
+                      (H_t || H_wTee) ? &eem_H_eef : 0);
 
   Vector6 F = (Vector6() << eem, eef).finished();
-  if (H_t || H_wTee) H_eef << eem_H_eef, gtsam::I_3x3;
+  if (H_t || H_wTee) H_eef << eem_H_eef, I_3x3;
   if (H_t) *H_t = H_eef * eef_H_wf * wf_H_t;
   if (H_wTee) {
     *(H_wTee) = H_eef * eef_H_wf * wf_H_dir * dir_H_wPem * wPem_H_wTee;
@@ -66,26 +64,23 @@ Vector6 CableTensionFactor::computeWrench(
 
 /******************************************************************************/
 Vector6 CableTensionFactor::computeWrench2(
-    double t, const Pose3 &wTee,
-    boost::optional<gtsam::Matrix &> H_t,
-    boost::optional<gtsam::Matrix &> H_wTee) const {
+    double t, const Pose3 &wTee, boost::optional<Matrix &> H_t,
+    boost::optional<Matrix &> H_wTee) const {
   // Jacobians: cable direction
-  gtsam::Matrix33 dir_H_wPem;
-  gtsam::Matrix36 wPem_H_wTee;
+  Matrix33 dir_H_wPem;
+  Matrix36 wPem_H_wTee;
   // Jacobians: force to wrench conversion
-  gtsam::Matrix61 wF_H_t;
-  gtsam::Matrix63 wF_H_dir;
+  Matrix61 wF_H_t;
+  Matrix63 wF_H_dir;
 
   // cable direction
   Point3 wPem = wTee.transformFrom(eePem_, H_wTee ? &wPem_H_wTee : 0);
-  Vector3 dir = gtsam::normalize(wPem - wPb_, H_wTee ? &dir_H_wPem : 0);
+  Vector3 dir = normalize(wPem - wPb_, H_wTee ? &dir_H_wPem : 0);
   // force->wrench
   Pose3 emTee = Pose3(wTee.rotation().inverse(), eePem_).inverse();
   Vector6 wF = (Vector6() << 0, 0, 0, -t * dir).finished();
-  if (H_t) wF_H_t = (gtsam::Matrix61() << 0, 0, 0, -dir).finished();
-  if (H_wTee)
-    wF_H_dir =
-        (gtsam::Matrix63() << gtsam::Z_3x3, -t * gtsam::I_3x3).finished();
+  if (H_t) wF_H_t = (Matrix61() << 0, 0, 0, -dir).finished();
+  if (H_wTee) wF_H_dir = (Matrix63() << Z_3x3, -t * I_3x3).finished();
   Vector6 F = emTee.AdjointMap().transpose() * wF;
   // TODO(gerry): find jacobian of adjoint map
   return F;
