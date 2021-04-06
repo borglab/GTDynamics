eea92cc4c80e1bcc6a6c51e9c6087bbdcbfd828e
Gerry Chen <gerry.chen2015@gmail.com>
2021-04-06 02:31:09 -0400

    animated plotting of tracking example

--- gtdynamics/cablerobot/src/gerry01_planar_tracking.py
+++ gtdynamics/cablerobot/src/gerry01_planar_tracking.py
@@ -18,11 +18,13 @@ from gtsam import Pose3, Rot3
 
 from cdpr_planar import Cdpr
 from cdpr_planar_controller import CdprController
-from cdpr_planar_sim import cdpr_sim
+from cdpr_planar_sim import CdprSimulator
+
+import cProfile
 
 def main():
     Tf = 1
-    dt = 0.05
+    dt = 0.01
     N = int(Tf / dt)
     cdpr = Cdpr()
     # set up controller
@@ -32,9 +34,11 @@ def main():
         for i in range(N)
     ]
     x_des[0] = x_des[1]
+    # initial state
     x0 = gtsam.Values()
     gtd.InsertPose(x0, cdpr.ee_id(), 0, x_des[0])
     gtd.InsertTwist(x0, cdpr.ee_id(), 0, np.zeros(6))
+    # controller
     controller = CdprController(cdpr,
                                 x0,
                                 x_des,
@@ -42,19 +46,39 @@ def main():
                                 Q=np.array([0, 1, 0, 1e3, 0, 1e3]),
                                 R=np.array([1e-3]))
     # run simulation
-    result = cdpr_sim(cdpr, x0, controller, dt=dt, N=N, verbose=True)
-    poses = [gtd.Pose(result, cdpr.ee_id(), k) for k in range(N)]
+    sim = CdprSimulator(cdpr, x0, controller, dt=dt)
+    result = sim.run(N=N, verbose=False)
 
-    # print(poses)
+    # extract useful variables as lists
+    poses = [gtd.Pose(result, cdpr.ee_id(), k) for k in range(N+1)]
+    posesxy = np.array([[pose.x() for pose in poses], [pose.z() for pose in poses]])
+    desposesxy = np.array([[pose.x() for pose in x_des], [pose.z() for pose in x_des]])
+    torques = np.array([[gtd.TorqueDouble(result, ji, k) for ji in range(4)] for k in range(N)])
 
-    plt.figure(1)
-    plt.plot([pose.x() for pose in x_des], [pose.z() for pose in x_des], 'r-')
-    plt.plot([pose.x() for pose in poses], [pose.z() for pose in poses], 'k--')
-    plt.plot([*cdpr.params.a_locs[:, 0], cdpr.params.a_locs[0, 0]],
-             [*cdpr.params.a_locs[:, 2], cdpr.params.a_locs[0, 2]], 'k-')
+    # plot utils
+    boxinds = np.array([0,1,2,3,0]).reshape(5,1)
+    def ee_coords(k):
+        return (posesxy[:, k]+cdpr.params.b_locs[boxinds, [0,2]]).T
+    frame_coords = cdpr.params.a_locs[boxinds, [0,2]].T
+    def cable_coords(k, ji):
+        return np.array([[cdpr.params.a_locs[ji][0], posesxy[0][k]+cdpr.params.b_locs[ji][0]],
+                        [cdpr.params.a_locs[ji][2], posesxy[1][k]+cdpr.params.b_locs[ji][2]]])
+    # plot
+    fig = plt.figure(1, figsize=(12,4))
+    # xy plot
+    plt.subplot(1,2,1)
+    plt.plot(*frame_coords, 'k-')
+    plt.plot(*desposesxy, 'r*') # desired trajectory
+    ltraj, = plt.plot(*posesxy, 'k-') # actual trajectory
+    lscables = plt.plot(np.zeros((2,4)), np.zeros((2,4)))
+    lee, = plt.plot(*ee_coords(0))
     plt.axis('equal')
-    plt.show()
+    plt.xlabel('x(m)');plt.ylabel('y(m)');plt.title('Trajectory')
+    # controls
+    plt.subplot(1,2,2)
+    lsctrl = plt.plot(np.arange(0,Tf,dt), torques)
+    plt.xlabel('time (s)');plt.ylabel('Cable tension (N)');plt.title('Control Inputs');
 
 
 if __name__ == '__main__':
-    main()
+    cProfile.run('main()')
