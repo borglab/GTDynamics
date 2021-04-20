94b30d40f7a318974538eedd76bb453e88605799
Gerry Chen <gerry.chen2015@gmail.com>
2021-04-20 17:08:33 -0400

    remove old add_objective functions

--- gtdynamics/factors/ObjectiveFactors.h
+++ gtdynamics/factors/ObjectiveFactors.h
@@ -25,135 +25,8 @@
 namespace gtdynamics {
 
 /**
- * @brief Add an pose objective to graph for the link i at time k.
- *
- * @param graph to add to.
- * @param pose target pose.
- * @param model noise model used in factor.
- * @param i The link id.
- * @param k Time step index (default 0).
- */
-void add_pose_objective(gtsam::NonlinearFactorGraph* graph,
-                        const gtsam::Pose3& pose,
-                        const gtsam::SharedNoiseModel& model, int i,
-                        int k = 0) {
-  graph->addPrior<gtsam::Pose3>(internal::PoseKey(i, k), pose, model);
-}
-
-/**
- * @brief Add pose/twist objective to graph for the link i at time k.
- *
- * @param graph to add to.
- * @param pose target pose.
- * @param pose_model noise model used for pose factor.
- * @param twist target twist.
- * @param twist_model noise model used for twist factor.
- * @param i The link id.
- * @param k Time step index (default 0).
- */
-void add_link_objective(gtsam::NonlinearFactorGraph* graph,
-                        const gtsam::Pose3& pose,
-                        const gtsam::SharedNoiseModel& pose_model,
-                        const gtsam::Vector6& twist,
-                        const gtsam::SharedNoiseModel& twist_model, int i,
-                        int k = 0) {
-  add_pose_objective(graph, pose, pose_model, i, k);
-  graph->addPrior<gtsam::Vector6>(internal::TwistKey(i, k), twist, twist_model);
-}
-
-/**
- * @brief Add twist+derivatives objective to graph for the link i at time k.
- *
- * @param graph to add to.
- * @param twist target twist.
- * @param twist_model noise model used for twist.
- * @param twist_acceleration target twist acceleration.
- * @param twist_acceleration_model noise model used for twist acceleration.
- * @param i The link id.
- * @param k Time step index (default 0).
- */
-void add_twist_objective(
-    gtsam::NonlinearFactorGraph* graph, const gtsam::Vector6& twist,
-    const gtsam::SharedNoiseModel& twist_model,  //
-    const gtsam::Vector6& twist_acceleration,
-    const gtsam::SharedNoiseModel& twist_acceleration_model,  //
-    int i, int k = 0) {
-  graph->addPrior<gtsam::Vector6>(internal::TwistKey(i, k), twist, twist_model);
-  graph->addPrior<gtsam::Vector6>(internal::TwistAccelKey(i, k),
-                                  twist_acceleration, twist_acceleration_model);
-}
-
-/**
- * @brief Add joint_angle objective to graph for joint j at time k.
- *
- * @param graph to add to.
- * @param joint_angle target joint= angle.
- * @param joint_angle_model noise model used for joint_angle.
- * @param j The joint id.
- * @param k Time step index (default 0).
- */
-void add_joint_objective(gtsam::NonlinearFactorGraph* graph, double joint_angle,
-                         const gtsam::SharedNoiseModel& joint_angle_model,  //
-                         int j, int k = 0) {
-  graph->addPrior<double>(internal::JointAngleKey(j, k), joint_angle,
-                          joint_angle_model);
-}
-
-/**
- * @brief Add joint_angle derivatives objective to graph for joint j at time k.
- *
- * @param graph to add to.
- * @param joint_velocity target velocity.
- * @param joint_velocity_model noise model used for velocity.
- * @param joint_acceleration target acceleration.
- * @param joint_acceleration_model noise model used for acceleration.
- * @param j The joint id.
- * @param k Time step index (default 0).
- */
-void add_joint_derivative_objectives(
-    gtsam::NonlinearFactorGraph* graph,  //
-    double joint_velocity,
-    const gtsam::SharedNoiseModel& joint_velocity_model,  //
-    double joint_acceleration,
-    const gtsam::SharedNoiseModel& joint_acceleration_model,  //
-    int j, int k = 0) {
-  graph->addPrior<double>(internal::JointVelKey(j, k), joint_velocity,
-                          joint_velocity_model);
-  graph->addPrior<double>(internal::JointAccelKey(j, k), joint_acceleration,
-                          joint_acceleration_model);
-}
-
-/**
- * @brief Add joint_angle+derivatives objective to graph for joint j at time k.
- *
- * @param graph to add to.
- * @param joint_angle target joint= angle.
- * @param joint_angle_model noise model used for joint_angle.
- * @param joint_velocity target velocity.
- * @param joint_velocity_model noise model used for velocity.
- * @param joint_acceleration target acceleration.
- * @param joint_acceleration_model noise model used for acceleration.
- * @param j The joint id.
- * @param k Time step index (default 0).
- */
-void add_joint_objectives(
-    gtsam::NonlinearFactorGraph* graph, double joint_angle,
-    const gtsam::SharedNoiseModel& joint_angle_model,  //
-    double joint_velocity,
-    const gtsam::SharedNoiseModel& joint_velocity_model,  //
-    double joint_acceleration,
-    const gtsam::SharedNoiseModel& joint_acceleration_model,  //
-    int j, int k = 0) {
-  add_joint_objective(graph, joint_angle, joint_angle_model, j, k);
-  add_joint_derivative_objectives(graph, joint_velocity, joint_velocity_model,
-                                  joint_acceleration, joint_acceleration_model,
-                                  j, k);
-}
-
-
-
-/**
- * @brief Proxy class idiom to add objectives to graph for link i at time k
+ * @brief Add objectives to graph for link i at time k using proxy class idiom
+ * for keyword argument -like syntax.
  * 
  * Example Usage:
  *  add_link_objectives(graph, id, k).pose(Pose3(), noise)
@@ -215,7 +88,8 @@ class add_link_objectives {
 };
 
 /**
- * @brief Proxy class idiom to add objectives to graph for joint j at time k
+ * @brief Add objectives to graph for joint j at time k using proxy class idiom
+ * for keyword argument -like syntax.
  * 
  * Example Usage:
  *  add_joint_objectives(graph, id, k).angle(0, noise)
@@ -279,10 +153,9 @@ void add_joints_at_rest_objectives(
     const gtsam::SharedNoiseModel& joint_velocity_model,
     const gtsam::SharedNoiseModel& joint_acceleration_model, int k = 0) {
   for (auto&& joint : robot.joints()) {
-    add_joint_derivative_objectives(graph,                        //
-                                    0, joint_velocity_model,      //
-                                    0, joint_acceleration_model,  //
-                                    joint->id(), k);
+    add_joint_objectives(graph, joint->id(), k)
+        .velocity(0, joint_velocity_model)
+        .acceleration(0, joint_acceleration_model);
   }
 }
 
--- tests/testSpiderWalking.cpp
+++ tests/testSpiderWalking.cpp
@@ -105,9 +105,9 @@ TEST(testSpiderWalking, WholeEnchilada) {
   // Add base goal objectives to the factor graph.
   auto base_link = robot.link("body");
   for (int k = 0; k <= K; k++) {
-    add_link_objective(&objectives, Pose3(Rot3(), Point3(0, 0.0, 0.5)),
-                       Isotropic::Sigma(6, 5e-5), gtsam::Z_6x1,
-                       Isotropic::Sigma(6, 5e-5), base_link->id(), k);
+    add_link_objectives(&objectives, base_link->id(), k)
+        .pose(Pose3(Rot3(), Point3(0, 0.0, 0.5)), Isotropic::Sigma(6, 5e-5))
+        .twist(gtsam::Z_6x1, Isotropic::Sigma(6, 5e-5));
   }
 
   // Add link and joint boundary conditions to FG.
@@ -127,7 +127,8 @@ TEST(testSpiderWalking, WholeEnchilada) {
   for (auto &&joint : robot.joints())
     if (joint->name().find("hip2") == 0)
       for (int k = 0; k <= K; k++)
-        add_joint_objective(&objectives, 2.5, prior_model, joint->id(), k);
+        add_joint_objectives(&objectives, joint->id(), k)
+            .angle(2.5, prior_model);
 
   // Regression test on objective factors
   EXPECT_LONGS_EQUAL(918, objectives.size());
