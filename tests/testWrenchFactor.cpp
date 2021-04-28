72c72ada469adbe428fd7f56d77a69706215837b
Frank Dellaert <dellaert@gmail.com>
2021-04-28 00:17:10 -0400

    Better test cases

--- tests/testWrenchFactor.cpp
+++ tests/testWrenchFactor.cpp
@@ -11,10 +11,6 @@
  * @author Yetong Zhang
  */
 
-#include "gtdynamics/factors/WrenchFactor.h"
-#include "gtdynamics/universal_robot/RobotModels.h"
-#include "gtdynamics/utils/values.h"
-
 #include <CppUnitLite/TestHarness.h>
 #include <gtsam/base/Testable.h>
 #include <gtsam/base/TestableAssertions.h>
@@ -27,123 +23,100 @@
 
 #include <iostream>
 
+#include "gtdynamics/factors/WrenchFactor.h"
+#include "gtdynamics/universal_robot/RobotModels.h"
+#include "gtdynamics/utils/values.h"
+
 using namespace gtdynamics;
-using internal::TwistKey;
-using internal::TwistAccelKey;
-using internal::WrenchKey;
 using internal::PoseKey;
+using internal::TwistAccelKey;
+using internal::TwistKey;
+using internal::WrenchKey;
 using namespace gtsam;
 
 namespace example {
 
 // R link example
-using simple_urdf_zero_inertia::robot;
+using simple_urdf::robot;
 
-auto inertia = robot.links()[0]->inertiaMatrix();
-Vector3 gravity(0, -9.8, 0);
+const Matrix6 inertia = robot.links()[0]->inertiaMatrix();
+const Vector3 gravity(0, -9.8, 0);
 
 noiseModel::Gaussian::shared_ptr cost_model =
     noiseModel::Gaussian::Covariance(I_6x6);
 
 }  // namespace example
 
-// Test wrench factor for stationary case with gravity
-TEST(WrenchFactor, error2) {
+constexpr double diffDelta = 1e-3;
+
+// Test wrench factor for non-stationary case
+TEST(WrenchFactor, Case1) {
   // Create all factors
   int id = 0;
-
+  const double M = example::inertia(3, 3);
   WrenchFactor factor(TwistKey(id), TwistAccelKey(id),
-                      {WrenchKey(id, 1), WrenchKey(id, 2)},
+                      {WrenchKey(id, 1), WrenchKey(id, 2)}, PoseKey(id),
+                      example::cost_model, example::inertia, example::gravity);
+  Values x;
+  InsertTwist(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
+  InsertTwistAccel(&x, id,
+                   (Vector(6) << 0, 0, 0, 0, -9.8 + 12 / M, 0).finished());
+  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, -1, 0, 10, 0).finished());
+  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, 1, 0, 2, 0).finished());
+  InsertPose(&x, id, Pose3(Rot3(), Point3(1, 0, 0)));
+
+  Vector6 actual_errors = factor.unwhitenedError(x);
+  Vector6 expected_errors = Z_6x1;
+  EXPECT(assert_equal(expected_errors, actual_errors, 1e-6));
+  // Make sure linearization is correct
+  EXPECT_CORRECT_FACTOR_JACOBIANS(factor, x, diffDelta, 1e-3);
+}
+
+// Test wrench factor compensating exactly for gravity
+TEST(WrenchFactor, Case2) {
+  // Create all factors
+  int id = 0;
+  const double M = example::inertia(3, 3);
+  WrenchFactor factor(TwistKey(id), TwistAccelKey(id),
+                      {WrenchKey(id, 1), WrenchKey(id, 2), WrenchKey(id, 3)},
                       PoseKey(id), example::cost_model, example::inertia,
                       example::gravity);
   Values x;
   InsertTwist(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
   InsertTwistAccel(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
-  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, -1, 0, 4.9, 0).finished());
-  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, 1, 0, 4.9, 0).finished());
+  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, 0, 0, M * 1, 0).finished());
+  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, 0, 0, M * 2, 0).finished());
+  InsertWrench(&x, id, 3, (Vector(6) << 0, 0, 0, 0, M * 6.8, 0).finished());
   InsertPose(&x, id, Pose3(Rot3(), Point3(1, 0, 0)));
 
   Vector6 actual_errors = factor.unwhitenedError(x);
   Vector6 expected_errors = Z_6x1;
   EXPECT(assert_equal(expected_errors, actual_errors, 1e-6));
   // Make sure linearization is correct
-  double diffDelta = 1e-7;
-  EXPECT_CORRECT_FACTOR_JACOBIANS(factor, x, diffDelta, 1e-3);
-}
-
-// Test wrench factor for stationary case with gravity
-TEST(WrenchFactor, error3) {
-  // Create all factors
-  int id = 0;
-
-  WrenchFactor factor(
-      TwistKey(id), TwistAccelKey(id),
-      {WrenchKey(id, 1), WrenchKey(id, 2), WrenchKey(id, 3)},
-      PoseKey(id), example::cost_model, example::inertia, example::gravity);
-  Values x;
-  InsertTwist(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
-  InsertTwistAccel(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
-  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, 0, 0, 1, 0).finished());
-  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, 0, 0, 2, 0).finished());
-  InsertWrench(&x, id, 3, (Vector(6) << 0, 0, 0, 0, 6.8, 0).finished());
-  InsertPose(&x, id, Pose3(Rot3(), Point3(1, 0, 0)));
-
-  Vector6 actual_errors = factor.unwhitenedError(x);
-  Vector6 expected_errors = Z_6x1;
-  EXPECT(assert_equal(expected_errors, actual_errors, 1e-6));
-  // Make sure linearization is correct
-  double diffDelta = 1e-7;
-  EXPECT_CORRECT_FACTOR_JACOBIANS(factor, x, diffDelta, 1e-3);
-}
-
-// Test wrench factor for stationary case with gravity
-TEST(WrenchFactor, error4) {
-  // Create all factors
-  int id = 0;
-
-  WrenchFactor factor(TwistKey(id), TwistAccelKey(id),
-                      {WrenchKey(id, 1), WrenchKey(id, 2),
-                       WrenchKey(id, 3), WrenchKey(id, 4)},
-                      PoseKey(id), example::cost_model, example::inertia,
-                      example::gravity);
-  Values x;
-  InsertTwist(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
-  InsertTwistAccel(&x, id, (Vector(6) << 0, 0, 0, 0, 0, 0).finished());
-  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, 0, 0, 1, 0).finished());
-  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, 0, 0, 1, 0).finished());
-  InsertWrench(&x, id, 3, (Vector(6) << 0, 0, 0, 0, 1, 0).finished());
-  InsertWrench(&x, id, 4, (Vector(6) << 0, 0, 0, 0, 6.8, 0).finished());
-  InsertPose(&x, id, Pose3(Rot3(), Point3(1, 0, 0)));
-
-  Vector6 actual_errors = factor.unwhitenedError(x);
-  Vector6 expected_errors = Z_6x1;
-  EXPECT(assert_equal(expected_errors, actual_errors, 1e-6));
-  // Make sure linearization is correct
-  double diffDelta = 1e-7;
   EXPECT_CORRECT_FACTOR_JACOBIANS(factor, x, diffDelta, 1e-3);
 }
 
 // Test wrench factor for non-zero twist case, zero joint angle
-TEST(WrenchFactor, error_nonzero) {
+TEST(WrenchFactor, NonzeroTwistCase) {
   // Create all factors
   int id = 0;
-
+  const double M = example::inertia(3, 3);
+  // gravity set to zero in this case
   WrenchFactor factor(TwistKey(id), TwistAccelKey(id),
-                      {WrenchKey(id, 1), WrenchKey(id, 2)},
-                      PoseKey(id), example::cost_model, example::inertia);
+                      {WrenchKey(id, 1), WrenchKey(id, 2)}, PoseKey(id),
+                      example::cost_model, example::inertia);
 
   Values x;
   InsertTwist(&x, id, (Vector(6) << 0, 0, 1, 0, 1, 0).finished());
   InsertTwistAccel(&x, id, (Vector(6) << 0, 0, 1, 0, 1, 0).finished());
-  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, 4, -1, 2, 0).finished());
-  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, -4, 0, -1, 0).finished());
+  InsertWrench(&x, id, 1, (Vector(6) << 0, 0, 4, M * -1, M * 2, 0).finished());
+  InsertWrench(&x, id, 2, (Vector(6) << 0, 0, -3, M * 0, -M, 0).finished());
   InsertPose(&x, id, Pose3(Rot3(), Point3(1, 0, 0)));
 
   Vector6 actual_errors = factor.unwhitenedError(x);
   Vector6 expected_errors = Z_6x1;
   EXPECT(assert_equal(expected_errors, actual_errors, 1e-6));
   // Make sure linearization is correct
-  double diffDelta = 1e-7;
   EXPECT_CORRECT_FACTOR_JACOBIANS(factor, x, diffDelta, 1e-3);
 }
 
