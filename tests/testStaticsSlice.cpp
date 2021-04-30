bb805eb37cd9d2672123cd78a43a8e48ae3215df
Frank Dellaert <dellaert@gmail.com>
2021-04-29 21:44:50 -0400

    Working solve

--- tests/testStaticsSlice.cpp
+++ tests/testStaticsSlice.cpp
@@ -13,9 +13,15 @@
 
 #include <CppUnitLite/TestHarness.h>
 // #include <gtdynamics/statics/Statics.h>
+#include <gtdynamics/factors/TorqueFactor.h>             // TODO: move
+#include <gtdynamics/factors/WrenchEquivalenceFactor.h>  // TODO: move
+#include <gtdynamics/factors/WrenchPlanarFactor.h>       // TODO: move
 #include <gtdynamics/kinematics/Kinematics.h>
 #include <gtdynamics/statics/StaticWrenchFactor.h>
 #include <gtdynamics/utils/Slice.h>
+#include <gtsam/linear/Sampler.h>
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 
 #include "contactGoalsExample.h"
 
@@ -27,15 +33,24 @@ using std::string;
 
 /// Noise models etc specific to Statics class
 struct StaticsParameters : public KinematicsParameters {
-  boost::optional<gtsam::Vector3> gravity;
+  boost::optional<gtsam::Vector3> gravity, planar_axis;
 
   using Isotropic = gtsam::noiseModel::Isotropic;
-  const gtsam::SharedNoiseModel fs_cost_model;  // statics cost model
+  const gtsam::SharedNoiseModel fs_cost_model,  // statics cost model
+      f_cost_model,                             // wrench equivalence factor
+      t_cost_model,                             // torque factor
+      planar_cost_model;                        // planar factor
 
   /// Constructor with default arguments
   StaticsParameters(
-      const boost::optional<gtsam::Vector3>& gravity = boost::none)
-      : gravity(gravity), fs_cost_model(Isotropic::Sigma(6, 1e-4)) {}
+      double sigma_dynamics = 1e-5,
+      const boost::optional<gtsam::Vector3>& gravity = boost::none,
+      const boost::optional<gtsam::Vector3>& planar_axis = boost::none)
+      : gravity(gravity),
+        planar_axis(planar_axis),
+        fs_cost_model(Isotropic::Sigma(6, 1e-4)),
+        f_cost_model(gtsam::noiseModel::Isotropic::Sigma(6, sigma_dynamics)),
+        t_cost_model(gtsam::noiseModel::Isotropic::Sigma(1, sigma_dynamics)) {}
 };
 
 /// Algorithms for Statics, i.e. kinematics + wrenches at rest
@@ -52,6 +67,72 @@ class Statics : public Kinematics {
       : Kinematics(robot, parameters), p_(parameters) {}
 
   /**
+   * Create keys for unkowns and initial values.
+   * TODO(frank): if we inherit, should we have *everything below us?
+   * @param slice Slice instance.
+   */
+  gtsam::Values initialValues(const Slice& slice,
+                              double gaussian_noise = 1e-6) const {
+    gtsam::Values values;
+    const auto k = slice.k;
+
+    auto sampler_noise_model =
+        gtsam::noiseModel::Isotropic::Sigma(6, gaussian_noise);
+    gtsam::Sampler sampler(sampler_noise_model);
+
+    // Initialize link poses.
+    for (auto&& link : robot_.links()) {
+      int i = link->id();
+      const gtsam::Vector6 xi = sampler.sample();
+      InsertPose(&values, i, k, link->wTcom().expmap(xi));
+    }
+
+    // Initialize wrenches and torques to 0.
+    for (auto&& joint : robot_.joints()) {
+      int j = joint->id();
+      InsertWrench(&values, joint->parent()->id(), j, k, gtsam::Z_6x1);
+      InsertWrench(&values, joint->child()->id(), j, k, gtsam::Z_6x1);
+      InsertTorque(&values, j, k, 0.0);
+    }
+
+    return values;
+  }
+
+  /// Graph with a WrenchEquivalenceFactor for each joint
+  gtsam::NonlinearFactorGraph wrenchEquivalenceFactors(const Slice& slice) {
+    gtsam::NonlinearFactorGraph graph;
+    for (auto&& joint : robot_.joints()) {
+      graph.emplace_shared<WrenchEquivalenceFactor>(
+          p_.f_cost_model, boost::static_pointer_cast<const JointTyped>(joint),
+          slice.k);
+    }
+    return graph;
+  }
+
+  /// Graph with a TorqueFactor for each joint
+  gtsam::NonlinearFactorGraph torqueFactors(const Slice& slice) {
+    gtsam::NonlinearFactorGraph graph;
+    for (auto&& joint : robot_.joints()) {
+      graph.emplace_shared<TorqueFactor>(
+          p_.t_cost_model, boost::static_pointer_cast<const JointTyped>(joint),
+          slice.k);
+    }
+    return graph;
+  }
+
+  /// Graph with a WrenchPlanarFactor for each joint
+  gtsam::NonlinearFactorGraph wrenchPlanarFactors(const Slice& slice) {
+    gtsam::NonlinearFactorGraph graph;
+    if (p_.planar_axis)
+      for (auto&& joint : robot_.joints()) {
+        graph.emplace_shared<WrenchPlanarFactor>(
+            p_.planar_cost_model, *p_.planar_axis,
+            boost::static_pointer_cast<const JointTyped>(joint), slice.k);
+      }
+    return graph;
+  }
+
+  /**
    * Create graph with only static balance factors.
    * TODO(frank): if we inherit, should we have *everything below us?
    * @param slice Slice instance.
@@ -60,21 +141,24 @@ class Statics : public Kinematics {
     gtsam::NonlinearFactorGraph graph;
     const auto k = slice.k;
 
-    for (auto&& link : robot_.links()) {
-      int i = link->id();
-      if (!link->isFixed()) {
-        const auto& connected_joints = link->joints();
-        std::vector<DynamicsSymbol> wrench_keys;
+    std::map<uint8_t, LinkSharedPtr> sorted;
+    for (auto&& link : robot_.links()) sorted.emplace(link->id(), link);
 
-        // Add wrench keys for joints.
-        for (auto&& joint : connected_joints)
-          wrench_keys.push_back(internal::WrenchKey(i, joint->id(), k));
+    for (auto&& kv : sorted) {
+      int i = kv.first;
+      auto link = kv.second;
+      if (link->isFixed()) continue;
+      const auto& connected_joints = link->joints();
+      std::vector<DynamicsSymbol> wrench_keys;
 
-        // add wrench factor for link
-        graph.emplace_shared<StaticWrenchFactor>(
-            wrench_keys, internal::PoseKey(link->id(), k), p_.fs_cost_model,
-            link->mass(), p_.gravity);
-      }
+      // Add wrench keys for joints.
+      for (auto&& joint : connected_joints)
+        wrench_keys.push_back(internal::WrenchKey(i, joint->id(), k));
+
+      // Add static wrench factor for link.
+      graph.emplace_shared<StaticWrenchFactor>(
+          wrench_keys, internal::PoseKey(link->id(), k), p_.fs_cost_model,
+          link->mass(), p_.gravity);
     }
 
     return graph;
@@ -86,18 +170,26 @@ class Statics : public Kinematics {
    * @param configuration A known kinematics configuration.
    */
   gtsam::Values solve(const Slice& slice, const gtsam::Values& configuration) {
-    gtsam::Values result;
+    auto graph = this->graph(slice);
+    GTD_PRINT(graph);
 
-    return result;
+    auto values = initialValues(slice);
+    GTD_PRINT(values);
+
+    // gtsam::LevenbergMarquardtOptimizer optimizer(graph, values,
+    //                                              p_.lm_parameters);
+    gtsam::GaussNewtonOptimizer optimizer(graph, values);
+    return optimizer.optimize();
   }
 };
 
 TEST(Phase, Statics) {
   // Load robot and establish contact/goal pairs
   using namespace contact_goals_example;
+  robot.print();
 
   // Create a slice.
-  const size_t k = 777;
+  const size_t k = 1;
   const Slice slice(k);
 
   // Instantiate statics algorithms
