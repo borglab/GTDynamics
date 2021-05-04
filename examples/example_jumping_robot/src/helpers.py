99dd37f26de07db5f71144595dedf4c2aa5fb748
lucas <lucas.tiziani@gmail.com>
2021-05-04 13:12:51 -0400

    simplify data path

--- examples/example_jumping_robot/src/helpers.py
+++ examples/example_jumping_robot/src/helpers.py
@@ -32,22 +32,24 @@ def mergeValues(values: gtsam.Values, values_add: gtsam.Values, overwrite=False)
     values.insert(new_values)
 
 
-def read_pressure(path_data: str, prefix_data: str) -> (np.array, np.array):
+def read_pressure(path_data: str) -> (np.array, np.array):
     ''' Import experimental pressure data '''
-    file_pressures = os.path.join(path_data, prefix_data, (prefix_data + ' pres_filt.txt'))
+    prefix_data = os.path.basename(os.path.normpath(path_data))
+    file_pressures = os.path.join(path_data, (prefix_data + ' pres_filt.txt'))
     data = np.loadtxt(file_pressures, dtype='float', comments='#', delimiter=',')
     time = data[:,0]
     pressure = data[:,1:]
     return time, pressure
 
 
-def read_marker_pix(path_data: str, prefix_data: str, dim_camera) -> (np.array, np.array):
+def read_marker_pix(path_data: str, dim_camera) -> (np.array, np.array):
     ''' Import marker pixel location measurements ''' 
-    file_vid_time = os.path.join(path_data, prefix_data, (prefix_data + ' vid_time.txt'))
+    prefix_data = os.path.basename(os.path.normpath(path_data))
+    file_vid_time = os.path.join(path_data, (prefix_data + ' vid_time.txt'))
     time = np.loadtxt(file_vid_time, dtype='float', comments='#', delimiter=',')
     time = time - time[0] # zero start time
 
-    file_marker_pix = os.path.join(path_data, prefix_data, (prefix_data + ' marker_pix.txt'))
+    file_marker_pix = os.path.join(path_data, (prefix_data + ' marker_pix.txt'))
     marker_pix = np.loadtxt(file_marker_pix, dtype='float', comments='#', delimiter=',')
     marker_pix[:,10:] = dim_camera[1] - marker_pix[:,10:] # flip y-axis
     marker_pix = np.fliplr(marker_pix) # swap x & y axes, reverse marker numbering
@@ -80,50 +82,49 @@ def interp_marker_pix(time: np.array, marker_pix: np.array, time_interp: np.arra
     return marker_pix_interp
 
 
-# use
-path_data = '/home/cs3630/Documents/system-id-data'
-prefix_data = '0p00_0p12_hipknee-80source 2021-04-05 11-43-47'
-dim_camera = [1920, 1080]    # GoPro is 1920 x 1080 (oriented horizontally)
-time_interp = np.arange(0,0.5,0.01)
+# # use
+# path_data = '/home/cs3630/Documents/system-id-data/0p00_0p12_hipknee-80source 2021-04-05 11-43-47'
+# dim_camera = [1920, 1080]    # GoPro is 1920 x 1080 (oriented horizontally)
+# time_interp = np.arange(0,0.5,0.01)
 
-time_exp, pressure_exp = read_pressure(path_data, prefix_data)
-time_vid, pix_vid = read_marker_pix(path_data, prefix_data, dim_camera)
+# time_exp, pressure_exp = read_pressure(path_data)
+# time_vid, pix_vid = read_marker_pix(path_data, dim_camera)
 
-pressure_interp = interp_pressure(time_exp, pressure_exp, time_interp)
-pix_interp = interp_marker_pix(time_vid, pix_vid, time_interp)
+# pressure_interp = interp_pressure(time_exp, pressure_exp, time_interp)
+# pix_interp = interp_marker_pix(time_vid, pix_vid, time_interp)
 
-# plot
-import matplotlib.pyplot as plt
+# # plot
+# import matplotlib.pyplot as plt
 
-# plot interpolated markers
-fig = plt.figure()
-for i in range(5):
-    ax = fig.add_subplot(1,5,i+1)
-    ax.plot(time_vid, pix_vid[:,i,0,0], 'r')
-    ax.plot(time_vid, pix_vid[:,i,0,1], 'g')
-    ax.plot(time_vid, pix_vid[:,i,1,0], 'b')
-    ax.plot(time_vid, pix_vid[:,i,1,1], 'k')
-    ax.plot(time_interp, pix_interp[:,i,0,0], 'r--')
-    ax.plot(time_interp, pix_interp[:,i,0,1], 'g--')
-    ax.plot(time_interp, pix_interp[:,i,1,0], 'b--')
-    ax.plot(time_interp, pix_interp[:,i,1,1], 'k--')
-plt.grid()
-plt.show()
+# # plot interpolated markers
+# fig = plt.figure()
+# for i in range(5):
+#     ax = fig.add_subplot(1,5,i+1)
+#     ax.plot(time_vid, pix_vid[:,i,0,0], 'r')
+#     ax.plot(time_vid, pix_vid[:,i,0,1], 'g')
+#     ax.plot(time_vid, pix_vid[:,i,1,0], 'b')
+#     ax.plot(time_vid, pix_vid[:,i,1,1], 'k')
+#     ax.plot(time_interp, pix_interp[:,i,0,0], 'r--')
+#     ax.plot(time_interp, pix_interp[:,i,0,1], 'g--')
+#     ax.plot(time_interp, pix_interp[:,i,1,0], 'b--')
+#     ax.plot(time_interp, pix_interp[:,i,1,1], 'k--')
+# plt.grid()
+# plt.show()
 
 
-# plot interpolated pressures
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.plot(time_exp, np.rad2deg(pressure_exp[:,0]), 'r')
-ax.plot(time_interp, np.rad2deg(pressure_interp[:,0]), 'r--')
-ax.plot(time_exp, np.rad2deg(pressure_exp[:,1]), 'g')
-ax.plot(time_interp, np.rad2deg(pressure_interp[:,1]), 'g--')
-ax.plot(time_exp, np.rad2deg(pressure_exp[:,2]), 'b')
-ax.plot(time_interp, np.rad2deg(pressure_interp[:,2]), 'b--')
-ax.plot(time_exp, np.rad2deg(pressure_exp[:,3]), 'k')
-ax.plot(time_interp, np.rad2deg(pressure_interp[:,3]), 'k--')
-ax.plot(time_exp, np.rad2deg(pressure_exp[:,4]), 'm')
-ax.plot(time_interp, np.rad2deg(pressure_interp[:,4]), 'm--')
-plt.grid()
-plt.show()
+# # plot interpolated pressures
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# ax.plot(time_exp, np.rad2deg(pressure_exp[:,0]), 'r')
+# ax.plot(time_interp, np.rad2deg(pressure_interp[:,0]), 'r--')
+# ax.plot(time_exp, np.rad2deg(pressure_exp[:,1]), 'g')
+# ax.plot(time_interp, np.rad2deg(pressure_interp[:,1]), 'g--')
+# ax.plot(time_exp, np.rad2deg(pressure_exp[:,2]), 'b')
+# ax.plot(time_interp, np.rad2deg(pressure_interp[:,2]), 'b--')
+# ax.plot(time_exp, np.rad2deg(pressure_exp[:,3]), 'k')
+# ax.plot(time_interp, np.rad2deg(pressure_interp[:,3]), 'k--')
+# ax.plot(time_exp, np.rad2deg(pressure_exp[:,4]), 'm')
+# ax.plot(time_interp, np.rad2deg(pressure_interp[:,4]), 'm--')
+# plt.grid()
+# plt.show()
         
