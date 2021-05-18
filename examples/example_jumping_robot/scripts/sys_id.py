651f9ceed3f5b1414445ed69193be485ec0a0e4b
lucas <lucas.tiziani@gmail.com>
2021-05-17 22:40:28 -0400

    fix experimental pressures

--- examples/example_jumping_robot/scripts/sys_id.py
+++ examples/example_jumping_robot/scripts/sys_id.py
@@ -10,7 +10,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 from src.jumping_robot import Actuator, JumpingRobot
-from src.jr_visualizer import visualize_jr
+from src.jr_visualizer import visualize_jr, make_plot
 from src.robot_graph_builder import RobotGraphBuilder
 from src.actuation_graph_builder import ActuationGraphBuilder
 from src.jr_graph_builder import JRGraphBuilder
@@ -48,29 +48,39 @@ def vertical_jump_sysid(jr, controls, init_values, step_phases, pixels_all_frame
 
 
     # debug
-    for f_idx in range(graph.size()):
-        factor = graph.at(f_idx)
-        if factor.error(init_values) > 1:
-            graph_tmp = gtsam.NonlinearFactorGraph()
-            graph_tmp.add(factor)
-            gtd.DynamicsGraph.printGraph(graph_tmp)
-            print("error", factor.error(init_values))
+    # for f_idx in range(graph.size()):
+    #     factor = graph.at(f_idx)
+    #     if factor.error(init_values) > 1:
+    #         graph_tmp = gtsam.NonlinearFactorGraph()
+    #         graph_tmp.add(factor)
+    #         gtd.DynamicsGraph.printGraph(graph_tmp)
+    #         print("error", factor.error(init_values))
 
     # values_keys = init_values.keys()
     # for key in gtd.KeySetToKeyVector(graph.keys()):
     #     if not key in values_keys:
     #         print(gtd.DynamicsSymbol(key))
 
-    # optimization
+
+    # values
     print("init error: ", graph.error(init_values))
+    print("Tube Diam:", init_values.atDouble(Actuator.TubeDiameterKey())*39.37, "in")
+    print("Knee Stiffness:", init_values.atDouble(Actuator.TendonStiffnessKey(1)), "N/m")
+    print("Hip Stiffness:", init_values.atDouble(Actuator.TendonStiffnessKey(2)), "N/m")
+    print("Joint Damping:", init_values.atDouble(Actuator.DampingKey()), "Nm/rad/s")
 
+    # optimization
+    params = gtsam.LevenbergMarquardtParams()
+    params.setVerbosityLM("SUMMARY")
+    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, init_values, params)
+    results = optimizer.optimize()
+    print("\nresult error: ", graph.error(results))
 
-    # params = gtsam.LevenbergMarquardtParams()
-    # params.setVerbosityLM("SUMMARY")
-    # optimizer = gtsam.LevenbergMarquardtOptimizer(graph, init_values, params)
-    # results = optimizer.optimize()
-    # print("result error: ", graph.error(results))
-
+    # values
+    print("Tube Diam:", results.atDouble(Actuator.TubeDiameterKey())*39.37, "in")
+    print("Knee Stiffness:", results.atDouble(Actuator.TendonStiffnessKey(1)), "N/m")
+    print("Hip Stiffness:", results.atDouble(Actuator.TendonStiffnessKey(2)), "N/m")
+    print("Joint Damping:", results.atDouble(Actuator.DampingKey()), "Nm/rad/s")
 
     # optimizer = gtsam.GaussNewtonOptimizer(graph, init_values)
     # results = optimizer.optimize()
@@ -86,7 +96,7 @@ def main():
     yaml_file_path = jr_folder + "/yaml/robot_config_2021-04-05.yaml"
     path_exp_data = jr_folder + "system-id-data/0p00_0p12_hipknee-80source 2021-04-05 11-43-47"
     
-    t_sim = 0.2
+    t_sim = 0.23 # sim up to just before takeoff
     dt = 0.005
 
     # create init config
@@ -100,8 +110,8 @@ def main():
     controls = JumpingRobot.create_controls(t_valve[0,:], t_valve[1,:])
 
     # simulate
-    # num_steps = int(t_sim/dt)
-    num_steps = 3
+    num_steps = int(t_sim/dt)
+    # num_steps = 10
     sim_values, step_phases = vertical_jump_simulation(jr, num_steps, dt, controls)
     print("step_phases", step_phases)
 
@@ -109,17 +119,18 @@ def main():
     dim_camera = [1920, 1080]
     time_mcu, pressures_mcu = read_pressure(path_exp_data)
     time_vid, pixels_vid = read_marker_pix(path_exp_data, dim_camera)
-    time_interp = np.arange(num_steps+1) * dt
+    time_interp = np.arange(num_steps) * dt
     pixels_interp = interp_marker_pix(time_vid, pixels_vid, time_interp)
-    pressures_interp = interp_pressure(time_mcu, pressures_mcu, time_interp)
+    pressures_interp = interp_pressure(time_mcu, pressures_mcu, time_interp) + 101.325 # (kPag)
 
     # system id
-    time_interp = np.arange(0, dt*len(step_phases), dt)
+    # time_interp = np.arange(0, dt*len(step_phases), dt)
     sysid_results = vertical_jump_sysid(jr, controls, sim_values, step_phases, 
         pixels_interp, pressures_interp)
 
+    # visualize
+    make_plot(sysid_results, jr, num_steps)
 
-    # visualize TODO
 
 
 if __name__ == "__main__":
