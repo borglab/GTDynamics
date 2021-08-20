6063edd9886182811705f9e772c89c0cefe8e313
Varun Agrawal <varagrawal@gmail.com>
2021-08-20 11:00:46 -0400

    small docs update

--- gtdynamics/factors/ForwardKinematicsFactor.h
+++ gtdynamics/factors/ForwardKinematicsFactor.h
@@ -74,7 +74,7 @@ class ForwardKinematicsFactor : public gtsam::BetweenFactor<gtsam::Pose3> {
    * @param end_link_name   The end link name whose pose to compute via FK.
    * @param joint_angles    gtsam::Values with joint angles for relevant joints.
    * @param model           The noise model for this factor.
-   * @param k               The discretized time index.
+   * @param k               The discrete time index.
    */
   ForwardKinematicsFactor(const Robot &robot,
                           const std::string &start_link_name,
