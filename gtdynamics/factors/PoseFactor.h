036ea0a1948e471845f65c9a7a1698c8e66ab862
Varun Agrawal <varagrawal@gmail.com>
2021-11-23 16:46:05 -0500

    file not properly formatted so fixing that

--- gtdynamics/factors/PoseFactor.h
+++ gtdynamics/factors/PoseFactor.h
@@ -46,30 +46,31 @@ using boost::assign::cref_list_of;
  * @param joint The joint connecting the two poses.
  * @param time The timestep at which this factor is defined.
  */
-inline gtsam::ExpressionFactor<gtsam::Vector6> PoseFactor(const gtsam::SharedNoiseModel &cost_model,
-             const JointConstSharedPtr &joint, int time) {
+inline gtsam::ExpressionFactor<gtsam::Vector6> PoseFactor(
+    const gtsam::SharedNoiseModel &cost_model, const JointConstSharedPtr &joint,
+    int time) {
   return gtsam::ExpressionFactor<gtsam::Vector6>(
       cost_model, gtsam::Vector6::Zero(), joint->poseConstraint(time));
 }
 
-  /**
-   * Create single factor relating this link's pose (COM) with previous one.
-   * Note: this function is provided for BW compatibility only, and will in time
-   * be replaced with EqualityConstraint.
-   * 
-   * @param wTp_key Key for parent link's CoM pose in world frame.
-   * @param wTc_key Key for child link's CoM pose in world frame.
-   * @param q_key Key for joint value.
-   * @param cost_model The noise model for this factor.
-   * @param joint The joint connecting the two poses
-   */
-inline gtsam::ExpressionFactor<gtsam::Vector6>  PoseFactor(DynamicsSymbol wTp_key, DynamicsSymbol wTc_key,
-             DynamicsSymbol q_key,
-             const gtsam::noiseModel::Base::shared_ptr &cost_model,
-             JointConstSharedPtr joint) {
+/**
+ * Create single factor relating this link's pose (COM) with previous one.
+ * Note: this function is provided for BW compatibility only, and will in time
+ * be replaced with EqualityConstraint.
+ *
+ * @param wTp_key Key for parent link's CoM pose in world frame.
+ * @param wTc_key Key for child link's CoM pose in world frame.
+ * @param q_key Key for joint value.
+ * @param cost_model The noise model for this factor.
+ * @param joint The joint connecting the two poses
+ */
+inline gtsam::ExpressionFactor<gtsam::Vector6> PoseFactor(
+    DynamicsSymbol wTp_key, DynamicsSymbol wTc_key, DynamicsSymbol q_key,
+    const gtsam::noiseModel::Base::shared_ptr &cost_model,
+    JointConstSharedPtr joint) {
   return gtsam::ExpressionFactor<gtsam::Vector6>(
-             cost_model, gtsam::Vector6::Zero(),
-             joint->poseConstraint(wTp_key.time()));
+      cost_model, gtsam::Vector6::Zero(),
+      joint->poseConstraint(wTp_key.time()));
 }
 
 }  // namespace gtdynamics
