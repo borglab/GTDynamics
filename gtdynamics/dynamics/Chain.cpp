b8a3f6663f9d880e8eb5fef758c558522604119f
Dan Barladeanu <barla4114@gmail.com>
2021-12-11 10:06:49 +0200

    added docs

--- gtdynamics/dynamics/Chain.cpp
+++ gtdynamics/dynamics/Chain.cpp
@@ -19,25 +19,45 @@ void Chain::monoidOperation(const Pose3 &aTb, const Matrix &bAj,
                             const Pose3 &bTc, const Matrix &cAk, 
                             Pose3 &aTc, Matrix &cAjk) {
     
+    // expect 6 rows in jacobians
+    if (bAj.rows() != 6 || cAk.rows() != 6) {
+      throw std::runtime_error(
+          "Jacobians should have 6 rows in SE(3)");
+    }
+
+    // Compose poses:
     aTc = aTb.compose(bTc);
     
+    // Adjoint the first Jacobian to the new end-effector frame C:
     Matrix c_Ad_b = bTc.inverse().AdjointMap();
+
+    // Multiply with first chain screw axes:
     Matrix mult = c_Ad_b * bAj;
+
+    // Create Matrix with correct number of columns:
     Matrix out(mult.rows(), mult.cols() + cAk.cols());
+    
+    // Fill Matrix: 
     out.leftCols(mult.cols()) = mult;
     out.rightCols(cAk.cols()) = cAk;
+    
     cAjk = out;
 }
 
 Chain Chain::compose(std::vector<Chain> &chain_vector){
     Pose3 T, Ti, Tres;
     Matrix A, Ai, Ares;
+
+    // Initialize from first chain in vector
     T = chain_vector[0].sMb();
     A = chain_vector[0].axes();
     int i = 1 ;
     for (i ; i < chain_vector.size() ; ++i) {
+      // Get pose and screw axes from chain
       Ti = chain_vector[i].sMb();
       Ai = chain_vector[i].axes();
+
+      // Perform monoid operation with composed chain we have
       monoidOperation(T,A,Ti,Ai,Tres,Ares);
       T = Tres;
       A = Ares;
@@ -47,46 +67,50 @@ Chain Chain::compose(std::vector<Chain> &chain_vector){
 
 Pose3 Chain::poe(const Vector &q, boost::optional<Pose3 &> fTe,
           boost::optional<Matrix &> J) {
-            
-if (q.size() != axes_.cols()) {
-  throw std::runtime_error(
-      "number of angles in q different from number of cols in axes");
+    // Check that input has good size    
+    if (q.size() != axes_.cols()) {
+      throw std::runtime_error(
+          "number of angles in q different from number of cols in axes");
+      }
+
+    // Build vector of exponential maps to use in poe
+    std::vector<Pose3> exp;
+    for (int i = 0; i < q.size(); ++i) {
+      exp.push_back(Pose3::Expmap(axes_.col(i) * q(i)));
     }
 
-  std::vector<Pose3> exp;
-  for (int i = 0; i < q.size(); ++i) {
-    exp.push_back(Pose3::Expmap(axes_.col(i) * q(i)));
-  }
 
-  Pose3 poe;
-  if (!J) {
-    poe = sMb_;
-    for (auto &expmap : exp){
-      poe = poe.compose(expmap);
+    Pose3 poe;
+    if (!J) {
+      // If J is not given, compute regular poe
+      poe = sMb_;
+      for (auto &expmap : exp){
+        poe = poe.compose(expmap);
+      }
+      if (fTe) {
+        poe = poe.compose(*fTe);
+      }
     }
-    if (fTe) {
-      poe = poe.compose(*fTe);
-    }
-  }
-  else {
-    Matrix Empty(6, 0), A, Ares;
-    Pose3 T, Tres;
-    T = sMb_;
-    A = Empty;
-    for (int j = 0; j < q.size(); ++j) {
-      monoidOperation(T,A,exp[j],axes_.col(j), Tres, Ares);
-      T = Tres;
-      A = Ares;
-    }
-    if (fTe) {
-      monoidOperation(T,A,*fTe,Empty, Tres, Ares);
-      T = Tres;
-      A = Ares;
-    }
-    poe = T;
-    *J = A;
+    else {
+      // Compute FK + Jacobian with monoid compose.
+      Matrix Empty(6, 0), A, Ares;
+      Pose3 T, Tres;
+      T = sMb_;
+      A = Empty;
+      for (int j = 0; j < q.size(); ++j) {
+        monoidOperation(T,A,exp[j],axes_.col(j), Tres, Ares);
+        T = Tres;
+        A = Ares;
+      }
+      if (fTe) {
+        monoidOperation(T,A,*fTe,Empty, Tres, Ares);
+        T = Tres;
+        A = Ares;
+      }
+      poe = T;
+      *J = A;
 
-  }
-  return poe;
+    }
+    return poe;
 }
 }
--- gtdynamics/dynamics/Chain.h
+++ gtdynamics/dynamics/Chain.h
@@ -34,22 +34,49 @@ namespace gtdynamics {
  */
 class Chain {
  private:
-  Pose3 sMb_; //rest pose of "body" with respect to "spatial" frame
-  Matrix axes_; //
+  Pose3 sMb_; //rest pose of "body" with respect to "spatial" frame.
+  Matrix axes_; //screw axes of all joints in the chain expressed in body frame.
 
  public:
   /// Constructor
   Chain(Pose3 &sMb, Matrix &axes) : sMb_(sMb), axes_(axes) {}
-
+  
+  /**
+   * This function performs the monoid operation on pose + jacobian pairs.
+   * The monoid operation returns the composed chain pose and jacobian.
+   * Detailed explanation in readme (chain.md)
+   * @param aTb ............. chain 1 pose
+   * @param bAj ............. chain 1 screw axes
+   * @param bTc ............. chain 2 pose
+   * @param cAk ............. chain 2 screw axes
+   * @param aTc ............. composed chain pose
+   * @param cAjk ............ composed chain jacobian
+   */
   static void monoidOperation(const Pose3 &aTb, const Matrix &bAj, const Pose3 &bTc,
                        const Matrix &cAk, Pose3 &aTc, Matrix &cAjk);
 
+  /**
+   * This function returns a chain object composed from all chains in 
+   * the input vector
+   * @param chain_vector ........... Vector containing chains to compose 
+   * @return ....................... Composed chain
+   */
   static Chain compose(std::vector<Chain> &chain_vector);
 
+  // Return sMb.
   inline const Pose3 &sMb() const { return sMb_; }
 
+  // Return screw axes.
   inline const Matrix &axes() const { return axes_; }
 
+
+  /**
+   * Perform forward kinematics given q, return Pose of end-effector.
+   * @param q ........... Input angles for all joints
+   * @param fTe ......... The end-effector pose with respect to final link (Optional)
+   * @param(out) J....... Empty matrix to be filled with the calculated Jacobian (Optional) 
+   * @return ............ Pose of the end-effector calculated using Product of Exponentials
+   */
   Pose3 poe(const Vector &q, boost::optional<Pose3 &> fTe = boost::none,
             boost::optional<Matrix &> J = boost::none);
 };
