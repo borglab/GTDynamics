d5bcbef4244f3447af2f570b76da845f76d46df0
Varun Agrawal <varagrawal@gmail.com>
2022-04-22 14:35:02 -0400

    add values.h include

--- gtdynamics/factors/JointMeasurementFactor.h
+++ gtdynamics/factors/JointMeasurementFactor.h
@@ -15,6 +15,7 @@
 #pragma once
 #include <gtdynamics/universal_robot/Joint.h>
 #include <gtdynamics/universal_robot/Link.h>
+#include <gtdynamics/utils/values.h>
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
 namespace gtdynamics {
