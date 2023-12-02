175136b8c26f9c6179ddd112d7a6b7a14fd99710
yetongumich <yetong@gatech.edu>
2023-12-02 17:14:14 -0500

    refactor visualization

--- scripts/python/yetong07_quad_viz_2d.py
+++ scripts/python/yetong07_quad_viz_2d.py
@@ -14,6 +14,7 @@ class PauseAnimation:
     def __init__(self):
         self.num_steps = len(data["time"])
 
+        # set subplots and ax
         self.fig, self.ax = plt.subplots(3, 3, figsize=(14, 9))
         self.ax_side = self.ax[0, 0]
         self.ax_top = self.ax[0, 1]
@@ -63,25 +64,7 @@ class PauseAnimation:
         self.ax_fcone.plot([0, 100], [0, 100], color='k', alpha=0.5)
         self.ax_fcone.plot([0, -100], [0, 100], color='k', alpha=0.5)
 
-        self.plots_side = []
-        self.plots_top = []
-        self.plots_jangles1 = []
-        self.plots_jangles2 = []
-        self.plots_jvels1 = []
-        self.plots_jvels2 = []
-        self.plots_jaccels1 = []
-        self.plots_jaccels2 = []
-        self.plots_torques1 = []
-        self.plots_torques2 = []
-        self.plots_cforce1 = []
-        self.plots_cforce2 = []
-        self.plots_torso_v1 = []
-        self.plots_torso_v2 = []
-        self.plots_torso_a1 = []
-        self.plots_torso_a2 = []
-        self.plots_fcone1 = []
-        self.plots_fcone2 = []
-
+        # general info
         self.end_point_names1 = [["fl_hip", "rl_hip"],
                                 ["fl_hip", "fl_upper"], ["fl_upper", "fl_lower"], ["fl_lower", "fl_lower_c"],
                                 ["rl_hip", "rl_upper"], ["rl_upper", "rl_lower"], ["rl_lower", "rl_lower_c"]]
@@ -100,103 +83,90 @@ class PauseAnimation:
         self.contact_names = [ "fl_lower_c", "rl_lower_c"]
         self.contact_colors = ['orange', 'b']
 
-        # max_q, min_q, max_v, min_v, max_a, min_a, max_T, min_T = 0, 0, 0, 0, 0, 0, 0, 0
-        # for joint_name in self.joint_names:
-        #     max_q = max(max_q, data[joint_name + "_q"].max())
-        #     min_q = min(min_q, data[joint_name + "_q"].min())
-        #     max_v = max(max_v, data[joint_name + "_v"].max())
-        #     min_v = min(min_v, data[joint_name + "_v"].min())
-        #     max_a = max(max_a, data[joint_name + "_a"].max())
-        #     min_a = min(min_a, data[joint_name + "_a"].min())
-        #     max_T = max(max_T, data[joint_name + "_T"].max())
-        #     min_T = min(min_T, data[joint_name + "_T"].min())
-        # self.ax_q.set_ylim(min_q, max_q)
-        # self.ax_v.set_ylim(min_v, max_v)
-        # self.ax_a.set_ylim(min_a, max_a)
-        # self.ax_T.set_ylim(min_T, max_T)
 
-        k = 0
+        self.time_plots_ax = []
+        self.time_plots_entry_name_x = []
+        self.time_plots_entry_name_y = []
+        self.time_plots_color = []
+        self.time_plots_label = []
+        self.time_plots1 = []
+        self.time_plots2 = []
+
+        self.step_plots_entry_name_x1 = []
+        self.step_plots_entry_name_x2 = []
+        self.step_plots_entry_name_y1 = []
+        self.step_plots_entry_name_y2 = []
+        self.step_plots_ax = []
+        self.step_plots_color = []
+        self.step_plots = []
+
+        # info of all step plots
         for (start_pt_name, end_pt_name), color in zip(self.end_point_names1, self.colors1):
-            x1 = data[start_pt_name+"_x"][k]
-            y1 = data[start_pt_name+"_y"][k]
-            z1 = data[start_pt_name+"_z"][k]
-            x2 = data[end_pt_name+"_x"][k]
-            y2 = data[end_pt_name+"_y"][k]
-            z2 = data[end_pt_name+"_z"][k]
-
-            frame = self.ax_side.plot((x1, x2), (z1, z2),
-                                    color=color, alpha=0.5, animated=True)[0]
-            self.plots_side.append(frame)
-
+            self.step_plots_ax += [self.ax_side]
+            self.step_plots_entry_name_x1 += [start_pt_name+"_x"]
+            self.step_plots_entry_name_x2 += [end_pt_name+"_x"]
+            self.step_plots_entry_name_y1 += [start_pt_name+"_z"]
+            self.step_plots_entry_name_y2 += [end_pt_name+"_z"]
+            self.step_plots_color += [color]
         for (start_pt_name, end_pt_name), color in zip(self.end_point_names2, self.colors2):
-            x1 = data[start_pt_name+"_x"][k]
-            y1 = data[start_pt_name+"_y"][k]
-            z1 = data[start_pt_name+"_z"][k]
-            x2 = data[end_pt_name+"_x"][k]
-            y2 = data[end_pt_name+"_y"][k]
-            z2 = data[end_pt_name+"_z"][k]
+            self.step_plots_ax += [self.ax_top]
+            self.step_plots_entry_name_x1 += [start_pt_name+"_x"]
+            self.step_plots_entry_name_x2 += [end_pt_name+"_x"]
+            self.step_plots_entry_name_y1 += [start_pt_name+"_y"]
+            self.step_plots_entry_name_y2 += [end_pt_name+"_y"]
+            self.step_plots_color += [color]
 
-            frame = self.ax_top.plot((x1, x2), (y1, y2),
-                                    color=color, alpha=0.5, animated=True)[0]
-            self.plots_top.append(frame)
-
+        # info of all timed plots
         for joint_name, color in zip(self.joint_names, self.joint_colors):
-            frame = self.ax_q.plot(data["time"][:1], data[joint_name+"_q"][:1],
-                         color = color, alpha=1, animated=True, label=joint_name)[0]
-            self.plots_jangles1.append(frame)
-            frame = self.ax_q.plot(data["time"][1:], data[joint_name+"_q"][1:],
-                         color = color, alpha=0.2, animated=True)[0]
-            self.plots_jangles2.append(frame)
-            frame = self.ax_v.plot(data["time"][:1], data[joint_name+"_v"][:1],
-                         color = color, alpha=1, animated=True)[0]
-            self.plots_jvels1.append(frame)
-            frame = self.ax_v.plot(data["time"][1:], data[joint_name+"_v"][1:],
-                         color = color, alpha=0.2, animated=True)[0]
-            self.plots_jvels2.append(frame)
-            frame = self.ax_a.plot(data["time"][:1], data[joint_name+"_a"][:1],
-                         color = color, alpha=1, animated=True)[0]
-            self.plots_jaccels1.append(frame)
-            frame = self.ax_a.plot(data["time"][1:], data[joint_name+"_a"][1:],
-                         color = color, alpha=0.2, animated=True)[0]
-            self.plots_jaccels2.append(frame)
-            frame = self.ax_T.plot(data["time"][:1], data[joint_name+"_T"][:1],
-                         color = color, alpha=1, animated=True)[0]
-            self.plots_torques1.append(frame)
-            frame = self.ax_T.plot(data["time"][1:], data[joint_name+"_T"][1:],
-                         color = color, alpha=0.2, animated=True)[0]
-            self.plots_torques2.append(frame)
+            self.time_plots_ax += [self.ax_q, self.ax_v, self.ax_a, self.ax_T]
+            self.time_plots_entry_name_x += ["time", "time", "time", "time"]
+            self.time_plots_entry_name_y += [joint_name+"_q", joint_name+"_v", joint_name+"_a", joint_name+"_T"]
+            self.time_plots_color += [color, color, color, color]
+            self.time_plots_label += [joint_name, joint_name, joint_name, joint_name]
+        for contact_name, color in zip(self.contact_names, self.contact_colors):
+            self.time_plots_ax += [self.ax_cforce, self.ax_fcone]
+            self.time_plots_entry_name_x += ["time", contact_name+"_fx"]
+            self.time_plots_entry_name_y += [contact_name+"_fz", contact_name+"_fz"]
+            self.time_plots_color += [color, color]
+            self.time_plots_label += [contact_name, contact_name]
+        self.time_plots_ax += [self.ax_torso_v, self.ax_torso_a]
+        self.time_plots_entry_name_x += ["time", "time"]
+        self.time_plots_entry_name_y += ["base_vz", "base_az"]
+        self.time_plots_color += ['r', 'b']
+        self.time_plots_label += ["torso_vz", "torso_az"]
+
+        # make time plots
+        for ax, entry_name_x, entry_name_y, color, label, in zip(self.time_plots_ax,
+                                                                 self.time_plots_entry_name_x,
+                                                                 self.time_plots_entry_name_y,
+                                                                 self.time_plots_color,
+                                                                 self.time_plots_label):
+            frame = ax.plot(data[entry_name_x][:1], data[entry_name_y][:1],
+                            color=color, alpha=1, animated=True, label=label)[0]
+            self.time_plots1.append(frame)
+            frame = ax.plot(data[entry_name_x][1:], data[entry_name_y][1:],
+                            color=color, alpha=0.2, animated=True)[0]
+            self.time_plots2.append(frame)
+            self.time_plots1
         self.ax_q.legend()
+
+        # make step plots
+        for ax, entry_name_x1, entry_name_x2, entry_name_y1, entry_name_y2, color in zip(self.step_plots_ax,
+                                                                                         self.step_plots_entry_name_x1,
+                                                                                         self.step_plots_entry_name_x2,
+                                                                                         self.step_plots_entry_name_y1,
+                                                                                         self.step_plots_entry_name_y2,
+                                                                                         self.step_plots_color):
+            x1 = data[entry_name_x1][0]
+            x2 = data[entry_name_x2][0]
+            y1 = data[entry_name_y1][0]
+            y2 = data[entry_name_y2][0]
+            frame = ax.plot((x1, x2), (y1, y2),
+                                    color=color, alpha=0.5, animated=True)[0]
+            self.step_plots.append(frame)
 
         self.title_k = self.ax_side.text(0.5, 1.2, str(k), bbox={
                           'facecolor': 'w', 'alpha': 0.5, 'pad': 5}, ha="center", animated=True)
-
-        for contact_name, color in zip(self.contact_names, self.contact_colors):
-            frame = self.ax_cforce.plot(data["time"][:1], data[contact_name+"_fz"][:1],
-                                        color=color, alpha=1, animated=True)[0]
-            self.plots_cforce1.append(frame)
-            frame = self.ax_cforce.plot(data["time"][1:], data[contact_name+"_fz"][1:],
-                                        color=color, alpha=0.2, animated=True)[0]
-            self.plots_cforce2.append(frame)
-            frame = self.ax_fcone.plot(data[contact_name+"_fx"][:1], data[contact_name+"_fz"][:1],
-                                       color=color, alpha=1, animated=True)[0]
-            self.plots_fcone1.append(frame)
-            frame = self.ax_fcone.plot(data[contact_name+"_fx"][1:], data[contact_name+"_fz"][1:],
-                                       color=color, alpha=0.2, animated=True)[0]
-            self.plots_fcone2.append(frame)
-
-        frame = self.ax_torso_v.plot(data["time"][:1], data["base_vz"][:1],
-                                     color='b', alpha=1, animated=True)[0]
-        self.plots_torso_v1.append(frame)
-        frame = self.ax_torso_v.plot(data["time"][1:], data["base_vz"][1:],
-                                     color='b', alpha=0.2, animated=True)[0]
-        self.plots_torso_v2.append(frame)
-
-        frame = self.ax_torso_a.plot(data["time"][:1], data["base_az"][:1],
-                                     color='r', alpha=1, animated=True)[0]
-        self.plots_torso_a1.append(frame)
-        frame = self.ax_torso_a.plot(data["time"][1:], data["base_az"][1:],
-                                     color='r', alpha=0.2, animated=True)[0]
-        self.plots_torso_a2.append(frame)
 
         self.animation = animation.FuncAnimation(
             self.fig, self.update, frames=self.num_steps, interval=300, blit=True)
@@ -214,100 +184,39 @@ class PauseAnimation:
     def get_all_plots(self):
         plots = []
         plots.append(self.title_k)
-        plots += self.plots_side
-        plots += self.plots_top
-        plots += self.plots_jangles1
-        plots += self.plots_jangles2
-        plots += self.plots_jvels1
-        plots += self.plots_jvels2
-        plots += self.plots_jaccels1
-        plots += self.plots_jaccels2
-        plots += self.plots_torques1
-        plots += self.plots_torques2
-        plots += self.plots_cforce1
-        plots += self.plots_cforce2
-        plots += self.plots_torso_v1
-        plots += self.plots_torso_v2
-        plots += self.plots_torso_a1
-        plots += self.plots_torso_a2
-        plots += self.plots_fcone1
-        plots += self.plots_fcone2
+        plots += self.step_plots
+        plots += self.time_plots1
+        plots += self.time_plots2
         return plots
 
     def update(self, k):
+        # update time plots
+        for plot, entry_name_x1, entry_name_x2, entry_name_y1, entry_name_y2 in zip(self.step_plots, 
+                                                                                    self.step_plots_entry_name_x1, 
+                                                                                    self.step_plots_entry_name_x2, 
+                                                                                    self.step_plots_entry_name_y1, 
+                                                                                    self.step_plots_entry_name_y2):
+            x1= data[entry_name_x1][k]
+            x2 = data[entry_name_x2][k]
+            y1 = data[entry_name_y1][k]
+            y2 = data[entry_name_y2][k]
+            plot.set_xdata([x1, x2])
+            plot.set_ydata([y1, y2])
 
-        for (start_pt_name, end_pt_name), plot1 in zip(self.end_point_names1, self.plots_side):
-            x1 = data[start_pt_name+"_x"][k]
-            y1 = data[start_pt_name+"_y"][k]
-            z1 = data[start_pt_name+"_z"][k]
-            x2 = data[end_pt_name+"_x"][k]
-            y2 = data[end_pt_name+"_y"][k]
-            z2 = data[end_pt_name+"_z"][k]
-            plot1.set_xdata([x1, x2])
-            plot1.set_ydata([z1, z2])
+        # update step plots
+        for plot1, plot2, entry_name_x, entry_name_y in zip(self.time_plots1,
+                                                            self.time_plots2,
+                                                            self.time_plots_entry_name_x,
+                                                            self.time_plots_entry_name_y):
+            plot1.set_xdata(data[entry_name_x][:k+1])
+            plot1.set_ydata(data[entry_name_y][:k+1])
+            plot2.set_xdata(data[entry_name_x][k:])
+            plot2.set_ydata(data[entry_name_y][k:])
 
-        for (start_pt_name, end_pt_name), plot2 in zip(self.end_point_names2, self.plots_top):
-            x1 = data[start_pt_name+"_x"][k]
-            y1 = data[start_pt_name+"_y"][k]
-            z1 = data[start_pt_name+"_z"][k]
-            x2 = data[end_pt_name+"_x"][k]
-            y2 = data[end_pt_name+"_y"][k]
-            z2 = data[end_pt_name+"_z"][k]
-            plot2.set_xdata([x1, x2])
-            plot2.set_ydata([y1, y2])
-
-        for joint_name, plot1, plot2 in zip(self.joint_names, self.plots_jangles1, self.plots_jangles2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data[joint_name+"_q"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data[joint_name+"_q"][k:])
-
-        for joint_name, plot1, plot2 in zip(self.joint_names, self.plots_jvels1, self.plots_jvels2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data[joint_name+"_v"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data[joint_name+"_v"][k:])
-
-        for joint_name, plot1, plot2 in zip(self.joint_names, self.plots_jaccels1, self.plots_jaccels2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data[joint_name+"_a"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data[joint_name+"_a"][k:])
-
-        for joint_name, plot1, plot2 in zip(self.joint_names, self.plots_torques1, self.plots_torques2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data[joint_name+"_T"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data[joint_name+"_T"][k:])
-
-
-        for contact_name, plot1, plot2 in zip(self.contact_names, self.plots_cforce1, self.plots_cforce2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data[contact_name+"_fz"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data[contact_name+"_fz"][k:])
-
-        for contact_name, plot1, plot2 in zip(self.contact_names, self.plots_fcone1, self.plots_fcone2):
-            plot1.set_xdata(data[contact_name+"_fx"][:k+1])
-            plot1.set_ydata(data[contact_name+"_fz"][:k+1])
-            plot2.set_xdata(data[contact_name+"_fx"][k:])
-            plot2.set_ydata(data[contact_name+"_fz"][k:])
-
-        for plot1, plot2 in zip(self.plots_torso_v1, self.plots_torso_v2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data["base_vz"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data["base_vz"][k:])
-        for plot1, plot2 in zip(self.plots_torso_a1, self.plots_torso_a2):
-            plot1.set_xdata(data["time"][:k+1])
-            plot1.set_ydata(data["base_az"][:k+1])
-            plot2.set_xdata(data["time"][k:])
-            plot2.set_ydata(data["base_az"][k:])
-
+        # update time step info
         self.title_k.set_text(str(k))
 
         return self.get_all_plots()
-        # self.plots.append(frame)
 
 pa = PauseAnimation()
 plt.show()
