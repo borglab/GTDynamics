476f5a16652d48fad1730a40fce22c634fef5979
Varun Agrawal <varagrawal@gmail.com>
2024-10-22 09:46:54 -0400

    update test fixture to return joint and links

--- tests/make_joint.h
+++ tests/make_joint.h
@@ -17,8 +17,16 @@
 #include <gtdynamics/universal_robot/Link.h>
 
 namespace gtdynamics {
-/// Create a joint with given rest transform cMp and screw-axis in child frame.
-JointConstSharedPtr make_joint(gtsam::Pose3 cMp, gtsam::Vector6 cScrewAxis) {
+/**
+ * Create a joint with given rest transform cMp and screw-axis in child frame.
+ *
+ * We return both the joint and its connected links so that
+ * the link pointers are alive in the test.
+ * This will be true in a Robot object since it holds
+ * both the joint and link pointers.
+ */
+std::pair<JointConstSharedPtr, std::vector<LinkSharedPtr>> make_joint(
+    gtsam::Pose3 cMp, gtsam::Vector6 cScrewAxis) {
   // create links
   std::string name = "l1";
   double mass = 100;
@@ -26,9 +34,8 @@ JointConstSharedPtr make_joint(gtsam::Pose3 cMp, gtsam::Vector6 cScrewAxis) {
   gtsam::Pose3 bMcom;
   gtsam::Pose3 bMl;
 
-  auto l1 = std::make_shared<Link>(Link(1, name, mass, inertia, bMcom, bMl));
-  auto l2 =
-      std::make_shared<Link>(Link(2, name, mass, inertia, cMp.inverse(), bMl));
+  auto l1 = std::make_shared<Link>(1, name, mass, inertia, bMcom, bMl);
+  auto l2 = std::make_shared<Link>(2, name, mass, inertia, cMp.inverse(), bMl);
 
   // create joint
   JointParams joint_params;
@@ -40,7 +47,11 @@ JointConstSharedPtr make_joint(gtsam::Pose3 cMp, gtsam::Vector6 cScrewAxis) {
   gtsam::Pose3 jMc = bMj.inverse() * l2->bMcom();
   gtsam::Vector6 jScrewAxis = jMc.AdjointMap() * cScrewAxis;
 
-  return std::make_shared<const HelicalJoint>(1, "j1", bMj, l1, l2, jScrewAxis,
+  auto joint = std::make_shared<HelicalJoint>(1, "j1", bMj, l1, l2, jScrewAxis,
                                               joint_params);
+  l1->addJoint(joint);
+  l2->addJoint(joint);
+  std::vector<LinkSharedPtr> links{l1, l2};
+  return {joint, links};
 }
 }  // namespace gtdynamics
